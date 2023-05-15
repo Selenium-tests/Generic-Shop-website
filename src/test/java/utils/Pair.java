63c0bfbf01abba6272e8af8fd34e62de5dd19fe1
aksi1984 <pablo_xmen@op.pl>
2023-05-15 19:03:09 +0200

    Pair enum implementation

---
+++ src/test/java/utils/Pair.java
@@ -0,0 +1,25 @@
+package utils;
+
+public class Pair<A, B> {
+
+    private A first;
+    private B second;
+
+    public Pair() { }
+
+    public Pair(A first, B second) {
+
+        this.first = first;
+        this.second = second;
+    }
+
+    public A first() {
+
+        return first;
+    }
+
+    public B second() {
+
+        return second;
+    }
+}
