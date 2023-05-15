4b35b70bccffc7ff1bc8e791dc28222f99aee777
aksi1984 <pablo_xmen@op.pl>
2023-05-15 19:03:30 +0200

    ProductPage class implementation

---
+++ src/test/java/pages/ProductPage.java
@@ -0,0 +1,29 @@
+package pages;
+
+import base.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class ProductPage extends BasePage {
+
+    public ProductPage(WebDriver driver) {
+
+        super(driver);
+    }
+
+    @FindBy(className = "product_title")
+    WebElement productTitle;
+
+    @FindBy(xpath = "//button[@name='add-to-cart']")
+    WebElement addToCartButton;
+
+    public void clickAddToCart() {
+
+        addToCartButton.click();
+    }
+    public String getProductTitle() {
+
+        return productTitle.getText();
+    }
+}
