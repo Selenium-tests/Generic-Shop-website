c455ff2fef4654147a5fdc08b68539e76ecf9744
aksi1984 <pablo_xmen@op.pl>
2023-05-15 19:04:23 +0200

    ResultsPage class implementation

---
+++ src/test/java/pages/components/ResultsPage.java
@@ -0,0 +1,36 @@
+package pages.components;
+
+import base.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+public class ResultsPage extends BasePage {
+
+    public ResultsPage(WebDriver driver) {
+
+        super(driver);
+    }
+
+    @FindBy(className = "page-header")
+    WebElement pageHeader;
+
+    @FindBy(className = "title")
+    List<WebElement> headers;
+
+    public int resultsSize() {
+
+        return headers.size();
+    }
+    public String getResult(int index) {
+
+        return headers.get(index).getText();
+    }
+
+    public boolean hasNoResults() {
+
+        return headers.isEmpty();
+    }
+}
