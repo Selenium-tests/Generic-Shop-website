9998181d8680387d6187a1ece4b1be7e907320f2
aksi1984 <pablo_xmen@op.pl>
2023-05-15 19:04:02 +0200

    RecentPostsSection class implementation

---
+++ src/test/java/pages/components/RecentPostsSection.java
@@ -0,0 +1,44 @@
+package pages.components;
+
+import base.BasePage;
+import com.google.j2objc.annotations.Weak;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.util.List;
+
+public class RecentPostsSection extends BasePage {
+
+    WebElement link;
+    String name;
+    public RecentPostsSection(WebDriver driver) {
+
+        super(driver);
+    }
+
+    @FindBy(className = "tyche-recent-posts")
+    List<WebElement> buttons;
+
+    public void setRecentPost(int index) {
+
+        link = buttons.get(index).findElement(By.xpath(".//a[@href]"));
+        name = link.getAttribute("text");
+    }
+
+    public void clickRecentButton() {
+
+        link.click();
+    }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public int getSize() {
+
+        return buttons.size();
+    }
+}
