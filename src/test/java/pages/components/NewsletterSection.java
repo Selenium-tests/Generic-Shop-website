dd7027c9346423535913a35206dc55b2ef1db847
aksi1984 <pablo_xmen@op.pl>
2023-05-15 19:02:45 +0200

    NewsletterSection class implementation

---
+++ src/test/java/pages/components/NewsletterSection.java
@@ -0,0 +1,52 @@
+package pages.components;
+
+import base.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class NewsletterSection extends BasePage {
+
+    public NewsletterSection(WebDriver driver) {
+
+        super(driver);
+    }
+
+    @FindBy(id = "es_txt_name")
+    WebElement name;
+
+    @FindBy(id = "es_txt_email")
+    WebElement email;
+
+    @FindBy(id = "es_txt_button")
+    WebElement subscribeButton;
+
+    @FindBy(id = "es_widget_msg")
+    WebElement newsletterMessage;
+
+    public void setName(String name) {
+
+        this.name.sendKeys(name);
+    }
+
+    public void setEmail(String email) {
+
+        this.email.sendKeys(email);
+    }
+
+    public void clickSubscribeButton() {
+
+        subscribeButton.click();
+    }
+
+    public void clearAll() {
+
+        name.clear();
+        email.clear();
+    }
+
+    public boolean isNewsletterMessageDisplayed() {
+
+        return newsletterMessage.isDisplayed();
+    }
+}
