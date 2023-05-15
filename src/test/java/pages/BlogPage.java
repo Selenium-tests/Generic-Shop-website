0b439f16973724860aee0bc152f429f08efb8b78
aksi1984 <pablo_xmen@op.pl>
2023-05-15 18:48:13 +0200

    BlogPage class implementation

---
+++ src/test/java/pages/BlogPage.java
@@ -0,0 +1,23 @@
+package pages;
+
+import base.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class BlogPage extends BasePage {
+
+    public BlogPage(WebDriver driver) {
+
+        super(driver);
+    }
+
+    @FindBy(className = "entry-title")
+    WebElement blogTitle;
+
+    public String getBlogTitle() {
+
+        return blogTitle.getText();
+    }
+
+}
