c0c25dd7efa6a809b1529b1ea250b33f1c2b9ea0
aksi1984 <pablo_xmen@op.pl>
2023-05-15 18:53:05 +0200

    Header class implementation

---
+++ src/test/java/pages/components/Header.java
@@ -0,0 +1,40 @@
+package pages.components;
+
+import base.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class Header extends BasePage {
+
+    private final SearchEngine searchEngine;
+    public Header(WebDriver driver) {
+
+        super(driver);
+
+        searchEngine = new SearchEngine(driver);
+    }
+
+    @FindBy(xpath = "//a[@class='site-title']")
+    protected WebElement logo;
+
+    @FindBy(xpath = ".//a[@href='https://skleptest.pl/my-account/']")
+    protected WebElement accountButton;
+
+    @FindBy(className = "top-cart")
+    protected WebElement cartButton;
+
+    public void clickLogo() {
+
+        logo.click();
+    }
+    public void clickCartButton() {
+
+        cartButton.click();
+    }
+
+    public void clickAccountButton() {
+
+        accountButton.click();
+    }
+}
