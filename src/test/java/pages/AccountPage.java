22259ca440a0c3ee67d1f46f6a790f6e38bbb6f4
aksi1984 <pablo_xmen@op.pl>
2023-05-21 19:01:11 +0200

    Add some methods opening pages

--- src/test/java/pages/AccountPage.java
+++ src/test/java/pages/AccountPage.java
@@ -1,17 +1,13 @@
 package pages;
 
 import base.BasePage;
-import com.opencsv.exceptions.CsvValidationException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.Test;
-
-import java.io.IOException;
 
 
 public class AccountPage extends BasePage {
+
     AccountPage(WebDriver driver) {
 
         super(driver);
@@ -35,76 +31,28 @@ public class AccountPage extends BasePage {
     @FindBy(xpath = "//a[@href='https://skleptest.pl/my-account/edit-address/billing']")
     WebElement billingAddressEditLink;
 
-    @BeforeClass
-    void openAccount() throws CsvValidationException, IOException {
-
-        //accountButton.click();
-        //CorrectLogin.perform(username, password, submit);
-    }
-
-    /*@Test
-    void linkToOrders() {
+    public void clickOrders() {
 
         orders.click();
-        //Assert.assertEquals(driver.getCurrentUrl(), getWEBSITE_URL() + "my-account/orders/");
     }
 
-    @Test
-    void linkToDownloads() {
+    void clickDownloads() {
 
         downloads.click();
-        //Assert.assertEquals(driver.getCurrentUrl(), getWEBSITE_URL() + "my-account/downloads/");
     }
 
-    @Test
-    void linkToDashboard() {
+    void clickDashboard() {
 
         dashboard.click();
-        //Assert.assertEquals(driver.getCurrentUrl(), getWEBSITE_URL() + "my-account/");
     }
 
-    @Test
-    void linkToAccountDetails() {
+    void clickAccountDetails() {
 
         accountDetails.click();
-        //Assert.assertEquals(driver.getCurrentUrl(), getWEBSITE_URL() + "my-account/customer-logout/");
     }
 
-    @Test
-    void linkToAddresses() {
+    void clickAddresses() {
 
         addresses.click();
-        //Assert.assertEquals(driver.getCurrentUrl(), getWEBSITE_URL() + "my-account/edit-address/");
     }
-
-    @Test(dependsOnMethods = {"linkToAddresses"})
-    void linkToBillingAddressEdit() {
-
-        billingAddressEditLink.click();
-    }
-
-    @Test
-    void choiceCountry() {
-
-    }
-
-    @Test
-    void correctCountryName() {
-
-    }
-
-    @Test
-    void incorrectCountryName() {
-
-    }
-
-    @Test
-    void incorrectFirstName() {
-
-    }
-
-    @Test
-    void incorrectLastName() {
-
-    }*/
 }
