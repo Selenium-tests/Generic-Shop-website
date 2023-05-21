dd4d8b43b474a23ce214f32423677b8d800e2214
aksi1984 <pablo_xmen@op.pl>
2023-05-21 21:58:15 +0200

    Removed unnecessary objects and methods

--- src/test/java/base/BasePage.java
+++ src/test/java/base/BasePage.java
@@ -2,14 +2,11 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
-import org.testng.asserts.SoftAssert;
 import java.time.Duration;
 
 public abstract class BasePage {
 
         protected WebDriver driver;
-        protected SoftAssert softAssert = new SoftAssert();
-        //final String chromeDriverFilePath = "C:\\Users\\Player One\\IdeaProjects\\untitled1\\drivers\\chromedriver.exe";
 
         protected BasePage(WebDriver driver) {
 
