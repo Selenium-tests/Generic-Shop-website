2c43b0bcc947c2f6da9eb986258286a740d5ade3
aksi1984 <pablo_xmen@op.pl>
2023-06-04 18:37:26 +0200

    CountryDropdownList class implementation

---
+++ src/test/java/pages/components/CountryDropdownList.java
@@ -0,0 +1,50 @@
+package pages.components;
+
+import base.BasePage;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+
+public class CountryDropdownList extends BasePage {
+
+    private final Robot robot;
+    public CountryDropdownList(WebDriver driver) throws AWTException {
+
+        super(driver);
+
+        robot = new Robot();
+    }
+
+    @FindBy(xpath = "//input[@class='select2-search__field']")
+    WebElement searchField;
+
+    @FindBy(className = "select2-selection__rendered")
+    WebElement countryButton;
+
+    @FindBy(xpath = "//li[@role='alert']")
+    WebElement alert;
+
+    public void clickCountryButton() {
+
+        countryButton.click();
+    }
+
+    public void setCountry(String countryName) {
+
+        searchField.sendKeys(countryName);
+        robot.keyPress(KeyEvent.VK_ENTER);
+    }
+
+    public String getCountry() {
+
+        return countryButton.getText();
+    }
+
+    public boolean isAlertDisplayed() {
+
+        return alert.isDisplayed();
+    }
+}
