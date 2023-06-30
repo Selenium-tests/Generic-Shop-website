af495c824d2cab3f62325d0cfc2085e5aa71c2ad
aksi1984 <pablo_xmen@op.pl>
2023-06-30 18:42:50 +0200

    TestListener class implementation

--- src/test/java/listener/TestListener.java
+++ src/test/java/listener/TestListener.java
@@ -1,2 +1,54 @@
-package listener;public class TestListener {
+package listener;
+
+import base.BaseTest;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+import utils.ExtentReportsManager;
+import utils.ScreenshotFileUtils;
+
+public class TestListener implements ITestListener {
+
+    @Override
+    public void onStart(ITestContext iTestContext) {
+
+        ExtentReportsManager.create(iTestContext.getSuite().getName());
+    }
+
+    @Override
+    public void onFinish(ITestContext iTestContext) {
+
+        System.out.println("End testing on suite " + iTestContext.getSuite().getName());
+        ExtentReportsManager.setEnvironment();
+        ExtentReportsManager.flush();
+    }
+
+    @Override
+    public void onTestStart(ITestResult iTestResult) {
+
+        System.out.println("Starting test with method: " + iTestResult.getMethod().getMethodName());
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult iTestResult) {
+
+        System.out.println("Test passed on method " + iTestResult.getMethod().getMethodName());
+        ExtentReportsManager.setTestPassed("Test passed on method: " + iTestResult.getMethod().getMethodName());
+    }
+
+    @Override
+    public void onTestFailure(ITestResult iTestResult) {
+
+        System.out.println("Test failed on method " + iTestResult.getMethod().getMethodName());
+        ExtentReportsManager.setTestFailed("Test failed on method: " + iTestResult.getMethod().getMethodName());
+
+        ScreenshotFileUtils.saveToFile(BaseTest.getDriver(), iTestResult.getMethod().getMethodName());
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult iTestResult) {
+
+        System.out.println("Test skipped on method " + iTestResult.getMethod().getMethodName());
+        ExtentReportsManager.setTestSkipped("Test skipped on method: " + iTestResult.getMethod().getMethodName());
+    }
 }
