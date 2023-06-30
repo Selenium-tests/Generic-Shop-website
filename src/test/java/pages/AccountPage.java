3dd21f848a84ce77b19b309712d5d7dc3bb2f137
aksi1984 <pablo_xmen@op.pl>
2023-06-30 18:02:35 +0200

    changed import of AddressForm class.

--- src/test/java/pages/AccountPage.java
+++ src/test/java/pages/AccountPage.java
@@ -4,7 +4,7 @@ import base.BasePage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-import pages.components.AddressForm;
+import pages.components.addressform.AddressForm;
 
 import java.awt.*;
 
