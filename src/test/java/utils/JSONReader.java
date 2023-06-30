e1ed56f407d25b32987decc76b1554907b5d3b03
aksi1984 <pablo_xmen@op.pl>
2023-06-30 18:32:15 +0200

    JSONReader class implementation

---
+++ src/test/java/utils/JSONReader.java
@@ -0,0 +1,76 @@
+package utils;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONArray;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JSONReader {
+    private static final String filePath = "./resources/test data/testdata.json";
+
+    private static JSONObject jsonObject;
+
+    private static String fileToString() {
+
+        try {
+
+            InputStream is = Files.newInputStream(Paths.get(filePath));
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(is));
+
+            String line = bufferedReader.readLine();
+            StringBuilder stringBuilder = new StringBuilder();
+
+            while (line != null) {
+
+                stringBuilder.append(line).append("\n");
+                line = bufferedReader.readLine();
+            }
+
+            return stringBuilder.toString();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void read() throws FileNotFoundException, JSONException {
+
+        jsonObject = new JSONObject(fileToString());
+    }
+
+    public static String[] get(String key, String node) throws JSONException {
+
+        Object object = jsonObject.get(key);
+        JSONObject jsonObject1 = (JSONObject) object;
+        JSONArray jsonArray = jsonObject1.getJSONArray(node);
+
+        String[] data = new String[jsonArray.length()];
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            data[i] = jsonArray.getString(i);
+        }
+
+        return data;
+    }
+
+    public static List<Pair<String, String>> get(String key, String node, Pair<String, String> params) throws JSONException {
+
+        Object object = jsonObject.get(key);
+        JSONObject jsonObject1 = (JSONObject) object;
+        JSONArray jsonArray = jsonObject1.getJSONArray(node);
+
+        List<Pair<String, String>> pairList = new ArrayList<>();
+
+        for (int i = 0; i < jsonArray.length(); i++) {
+            pairList.add(new Pair<>( jsonArray.getJSONObject(i).getString(params.first()),
+                    jsonArray.getJSONObject(i).getString(params.second())));
+        }
+
+        return pairList;
+    }
+}
