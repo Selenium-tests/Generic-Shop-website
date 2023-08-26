436a4b1de708907d10852ef194560cfbc7897a2c
aksi1984 <pablo_xmen@op.pl>
2023-08-26 22:32:18 +0200

    Removed getTitle() method

--- src/test/java/base/BasePage.java
+++ src/test/java/base/BasePage.java
@@ -2,25 +2,11 @@ package base;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
-import java.time.Duration;
 
 public abstract class BasePage {
 
-        protected WebDriver driver;
-
         protected BasePage(WebDriver driver) {
 
-                this.driver = driver;
-
                 PageFactory.initElements(driver, this);
-        }
-        public String getTitle() {
-
-                return driver.getTitle();
-        }
-
-        public void wait(int seconds) {
-
-                driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(seconds));
         }
 }
