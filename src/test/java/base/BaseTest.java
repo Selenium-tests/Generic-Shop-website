bfcccd0b517be30faf1da9ae859797961b4b3992
aksi1984 <pablo_xmen@op.pl>
2023-08-26 22:33:33 +0200

    Added login() and getLoginPage() methods

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -6,14 +6,19 @@ import org.json.JSONException;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import pages.LoginPage;
+import pages.components.header.Header;
 import utils.JSONReader;
+import utils.Pair;
 
 import java.io.FileNotFoundException;
-import java.util.concurrent.TimeUnit;
+import java.time.Duration;
+import java.util.List;
 
 public class BaseTest {
 
     private static WebDriver driver;
+    private LoginPage loginPage;
 
     @BeforeClass
     public void startDriver() throws FileNotFoundException, JSONException {
@@ -22,10 +27,12 @@ public class BaseTest {
 
         driver = DriverType.createDriver(Browser.CHROME);
         driver.navigate().to("http://skleptest.pl/");
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
 
         JSONReader.read();
+
+        loginPage = new LoginPage(driver);
     }
 
     protected void back() {
@@ -40,6 +47,22 @@ public class BaseTest {
     public static WebDriver getDriver() {
 
         return driver;
+    }
+
+    public void login(List<Pair<String, String>> data) {
+
+        Header header = new Header(driver);
+
+        header.clickAccountButton();
+
+        loginPage.setUsername(data.get(0).first());
+        loginPage.setPassword(data.get(0).second());
+        loginPage.clickLoginButton();
+
+    }
+    public LoginPage getLoginPage() {
+
+        return loginPage;
     }
 
     @AfterClass
