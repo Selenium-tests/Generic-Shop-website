4a275ef37734408e43d01a7c712c2f589294d36d
aksi1984 <pablo_xmen@op.pl>
2023-09-26 22:27:57 +0200

    Created AddressFormBase class

---
+++ src/test/java/pages/components/addressform/AddressFormBase.java
@@ -0,0 +1,81 @@
+package pages.components.addressform;
+
+import base.BasePage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import java.awt.*;
+import java.util.List;
+
+public class AddressFormBase extends BasePage {
+
+    private final String prefix;
+    private final CountryDropdownList countryDropdownList;
+
+    public AddressFormBase(WebDriver driver, String prefix) throws AWTException {
+
+        super(driver);
+
+        this.prefix = prefix;
+        countryDropdownList = new CountryDropdownList(driver);
+    }
+
+    @FindBy(xpath = ".//div[@class='woocommerce-address-fields']")
+    WebElement addressFields;
+
+    @FindBy(xpath = "//ul[@class='woocommerce-error']")
+    List<WebElement> errorMessage;
+
+    protected void fill(WebElement element, String data) {
+
+        element.clear();
+        element.sendKeys(data);
+    }
+
+    public void setFirstName(String firstName) {
+
+        fill(addressFields.findElement(By.id(prefix + "_first_name")), firstName);
+    }
+
+    public void setLastName(String lastName) {
+
+        fill(addressFields.findElement(By.id(prefix + "_last_name")), lastName);
+    }
+
+    public void setCompany(String company) {
+
+        fill(addressFields.findElement(By.id(prefix + "_company")), company);
+    }
+
+    public void setAddress_1(String address) {
+
+        fill(addressFields.findElement(By.id(prefix + "_address_1")), address);
+    }
+
+    public void setAddress_2(String address) {
+
+        fill(addressFields.findElement(By.id(prefix + "_address_2")), address);
+    }
+
+    public void setCity(String city) {
+
+        fill(addressFields.findElement(By.id(prefix + "_city")), city);
+    }
+
+    public void setState(String state) {
+
+        fill(addressFields.findElement(By.id(prefix + "_state")), state);
+    }
+
+    public void setPostcode(String postcode) {
+
+        fill(addressFields.findElement(By.id(prefix + "_postcode")), postcode);
+    }
+
+    public CountryDropdownList getCountryDropdownList() {
+
+        return countryDropdownList;
+    }
+}
