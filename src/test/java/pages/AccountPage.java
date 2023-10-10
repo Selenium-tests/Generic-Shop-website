dc8c597006277c7c46ba5b10419a4903885727b4
aksi1984 <pablo_xmen@op.pl>
2023-10-11 00:57:55 +0200

    Added the AccountNavigation class instance

--- src/test/java/pages/AccountPage.java
+++ src/test/java/pages/AccountPage.java
@@ -4,84 +4,29 @@ import base.BasePage;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
-
-
-import java.awt.*;
-
+import pages.account.AccountNavigation;
 
 public class AccountPage extends BasePage {
 
-    public AccountPage(WebDriver driver) throws AWTException {
+    private final AccountNavigation accountNavigation;
+
+    public AccountPage(WebDriver driver) {
 
         super(driver);
+
+        accountNavigation = new AccountNavigation(driver);
     }
-
-    @FindBy(linkText = "Orders")
-    WebElement orders;
-
-    @FindBy(linkText = "Downloads")
-    WebElement downloads;
 
     @FindBy(linkText = "Dashboard")
     WebElement dashboard;
-
-    @FindBy(linkText = "Account details")
-    WebElement accountDetails;
-
-    @FindBy(linkText = "Addresses")
-    WebElement addresses;
-
-    @FindBy(linkText = "Logout")
-    WebElement logout;
-
-    @FindBy(xpath = "//a[@href='https://skleptest.pl/my-account/edit-address/billing']")
-    WebElement billingAddressEditLink;
-
-    @FindBy(xpath = "//a[@href='https://skleptest.pl/my-account/edit-address/shipping']")
-    WebElement shippingAddressEditLink;
-
-    public void clickOrdersLink() {
-
-        orders.click();
-    }
-
-    public void clickDownloadsLink() {
-
-        downloads.click();
-    }
-
-    public void clickDashboardLink() {
-
-        dashboard.click();
-    }
-
-    public void clickLogoutLink() {
-
-        logout.click();
-    }
-
-    public void clickAccountDetailsLink() {
-
-        accountDetails.click();
-    }
 
     public boolean isDashboardLinkDisplayed() {
 
         return dashboard.isDisplayed();
     }
 
-    public void clickAddressesLink() {
+    public AccountNavigation getAccountNavigation() {
 
-        addresses.click();
-    }
-
-    public void clickBillingAddressEditLink() {
-
-        billingAddressEditLink.click();
-    }
-
-    public void clickShippingAddressEditLink() {
-
-        shippingAddressEditLink.click();
+        return accountNavigation;
     }
 }
