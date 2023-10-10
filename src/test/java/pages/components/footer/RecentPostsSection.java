94e463b14de6fbed6d5db4389af7aaf8f347c357
aksi1984 <pablo_xmen@op.pl>
2023-10-10 23:46:00 +0200

    Removed unnecessary methods

--- src/test/java/pages/components/footer/RecentPostsSection.java
+++ src/test/java/pages/components/footer/RecentPostsSection.java
@@ -1,44 +1,24 @@
 package pages.components.footer;
 
 import base.BasePage;
-import com.google.j2objc.annotations.Weak;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-import java.util.List;
 
 public class RecentPostsSection extends BasePage {
 
-    WebElement link;
-    String name;
     public RecentPostsSection(WebDriver driver) {
 
         super(driver);
     }
 
-    @FindBy(className = "tyche-recent-posts")
-    List<WebElement> buttons;
+    @FindBy(id = "tyche_recent-3")
+    WebElement container;
 
-    public void setRecentPost(int index) {
+    public void clickLink(String linkText) {
 
-        link = buttons.get(index).findElement(By.xpath(".//a[@href]"));
-        name = link.getText();
-    }
-
-    public void clickRecentButton() {
-
-        link.click();
-    }
-
-    public String getName() {
-
-        return name;
-    }
-
-    public int getSize() {
-
-        return buttons.size();
+        container.findElement(By.linkText(linkText)).click();
     }
 }
