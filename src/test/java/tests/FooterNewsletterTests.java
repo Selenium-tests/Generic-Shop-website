7a71ec1b5d757435803d5914e986531958aeeffa
aksi1984 <pablo_xmen@op.pl>
2023-10-10 23:38:17 +0200

    Created tests for newsletter form

---
+++ src/test/java/tests/FooterNewsletterTests.java
@@ -0,0 +1,77 @@
+package tests;
+
+import base.BaseTest;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.NoAlertPresentException;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pages.components.footer.Footer;
+import provider.MyDataProvider;
+import utils.Pair;
+
+public class FooterNewsletterTests extends BaseTest {
+
+    private Footer footer;
+
+    @BeforeMethod
+    public void create() {
+
+        footer = new Footer(getDriver());
+    }
+
+    private void fill(Pair<String, String> data) {
+
+        footer.getNewsletterForm().clearAll();
+        footer.getNewsletterForm().setName(data.first());
+        footer.getNewsletterForm().setEmail(data.second());
+        footer.getNewsletterForm().clickSubscribeButton();
+    }
+
+    private void check(String expectedAlertText) {
+
+        try {
+
+            Alert alert = getDriver().switchTo().alert();
+
+            Assert.assertEquals(alert.getText(), expectedAlertText);
+
+        } catch (NoAlertPresentException e) {
+
+            Assert.fail(e.getMessage());
+        }
+    }
+
+    @Test(dataProvider = "newsletterCorrectCredentials", dataProviderClass = MyDataProvider.class)
+    public void correctCredentials(Pair<String, String> data) throws InterruptedException {
+
+        fill(data);
+
+        Thread.sleep(2000);
+
+        Assert.assertTrue(footer.getNewsletterForm().isMessageDisplayed());
+        Assert.assertEquals(footer.getNewsletterForm().getMessageText(), "Succesfully Subscribed");
+    }
+
+    @Test(dataProvider = "newsletterBlankUsernameField", dataProviderClass = MyDataProvider.class)
+    public void blankUsernameField(Pair<String, String> data) {
+
+        fill(data);
+
+        Assert.assertTrue(footer.getNewsletterForm().isMessageDisplayed());
+    }
+
+    @Test(dataProvider = "newsletterIncorrectEmailFormat", dataProviderClass = MyDataProvider.class)
+    public void incorrectEmailFormat(Pair<String, String> data) {
+
+        fill(data);
+        check("Please enter correct email address");
+    }
+
+    @Test(dataProvider = "newsletterBlankEmailField", dataProviderClass = MyDataProvider.class)
+    public void blankEmailField(Pair<String, String> data) {
+
+        fill(data);
+        check("Please enter email address");
+    }
+}
