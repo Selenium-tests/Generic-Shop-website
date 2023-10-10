e8106fb7a8be901ae5b76ea1ca3b05d4628e35a8
aksi1984 <pablo_xmen@op.pl>
2023-10-10 23:41:57 +0200

    Removed unnecessary methods

--- src/test/java/pages/components/footer/TagsSection.java
+++ src/test/java/pages/components/footer/TagsSection.java
@@ -6,39 +6,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-import java.util.List;
-
 public class TagsSection extends BasePage {
 
-    WebElement button;
-    String name;
     public TagsSection(WebDriver driver) {
 
         super(driver);
     }
 
     @FindBy(className = "tagcloud")
-    WebElement buttons;
+    WebElement tagCloud;
 
-    public void setTag(int index) {
+    public void clickLink(String linkText) {
 
-        List<WebElement> links = buttons.findElements(By.className("tag-cloud-link"));
-        button = links.get(index);
-        name = button.getText();
-    }
-
-    public void clickTagButton() {
-
-        button.click();
-    }
-
-    public String getName() {
-
-        return name;
-    }
-
-    public int getSize() {
-
-        return buttons.findElements(By.className("tag-cloud-link")).size();
+        tagCloud.findElement(By.linkText(linkText)).click();
     }
 }
