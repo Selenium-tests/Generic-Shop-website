bf084f91b257ba933038e5dde6f31b4c7b7a04fa
aksi1984 <pablo_xmen@op.pl>
2023-10-24 16:47:01 +0200

    Created the AddressFormData class

---
+++ src/main/java/qa/utils/AddressFormData.java
@@ -0,0 +1,97 @@
+package qa.utils;
+
+public class AddressFormData {
+
+    private String country;
+    private String firstName;
+    private String lastName;
+    private String companyName;
+    private String address1;
+    private String address2;
+    private String city;
+    private String postcode;
+    private String phone;
+    private String email;
+
+    public AddressFormData() { }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public void setAddress1(String address1) {
+        this.address1 = address1;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setPostcode(String postcode) {
+        this.postcode = postcode;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public String getAddress1() {
+        return address1;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getPostcode() {
+        return postcode;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
