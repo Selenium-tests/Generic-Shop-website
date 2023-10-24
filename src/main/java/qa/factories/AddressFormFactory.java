11dc6bc0bad1b72f2be507c6a18a83c7da4d75e7
aksi1984 <pablo_xmen@op.pl>
2023-10-24 16:47:35 +0200

    Created the AddressFormFactory class

---
+++ src/main/java/qa/factories/AddressFormFactory.java
@@ -0,0 +1,29 @@
+package qa.factories;
+
+import org.openqa.selenium.WebDriver;
+import qa.pageobject.addressform.AddressForm;
+import qa.utils.AddressFormData;
+
+
+public class AddressFormFactory {
+
+    public static AddressForm correctData(AddressFormData data, WebDriver driver) {
+
+        AddressForm addressForm = new AddressForm(driver);
+
+        addressForm.getCountryDropdownList().clickCountryButton();
+        addressForm.getCountryDropdownList().setCountry(data.getCountry());
+        addressForm.getCountryDropdownList().pressEnter();
+        addressForm.setFirstName(data.getFirstName());
+        addressForm.setLastName(data.getLastName());
+        addressForm.setCompany(data.getCompanyName());
+        addressForm.setAddress_1(data.getAddress1());
+        addressForm.setAddress_2(data.getAddress2());
+        addressForm.setCity(data.getCity());
+        addressForm.setPostcode(data.getPostcode());
+        addressForm.setPhone(data.getPhone());
+        addressForm.setEmail(data.getEmail());
+
+        return addressForm;
+    }
+}
