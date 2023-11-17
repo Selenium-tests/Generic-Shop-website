a1afbf4b134f4c7edcf06040d09889692d93e9fb
aksi1984 <pablo_xmen@op.pl>
2023-11-17 23:28:04 +0100

    Added initializing the driver with a factory

--- src/main/java/qa/base/BaseTest.java
+++ src/main/java/qa/base/BaseTest.java
@@ -1,27 +1,21 @@
 package qa.base;
 
-
 import org.json.JSONException;
 import org.json.simple.parser.ParseException;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-import qa.enums.Browser;
-import qa.driver.DriverType;
-import qa.pageobject.LoginPage;
-import qa.pageobject.header.Header;
+import qa.browsermanager.BrowserManager;
+import qa.driver.ChromeDriverFactory;
+import qa.driver.WebDriverFactory;
 import qa.utils.JSONReader;
 import java.io.IOException;
-import java.time.Duration;
+
 
 public class BaseTest {
 
     private static WebDriver driver;
-    private WebDriverWait webDriverWait;
-    private LoginPage loginPage;
 
     @BeforeClass
     public void readJSONFile() throws JSONException, IOException, ParseException {
@@ -32,57 +26,25 @@ public class BaseTest {
     @BeforeMethod
     public void startDriver() {
 
-        System.out.println("Starting driver...");
+        WebDriverFactory factory = new ChromeDriverFactory();
+        driver = factory.createWebDriver();
 
-        driver = DriverType.createDriver(Browser.CHROME);
-        driver.navigate().to("http://skleptest.pl/");
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-        driver.manage().window().maximize();
-
-        loginPage = new LoginPage(driver);
-
-        webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(30));
+        BrowserManager.openBrowser(driver, "http://skleptest.pl/");
     }
 
     @AfterMethod
     public void quitDriver() {
 
-        System.out.println("Quit driver");
-        driver.quit();
+        BrowserManager.closeBrowser(driver);
     }
 
-    protected void back() {
-
-        driver.navigate().back();
-    }
     public void setDriver(WebDriver driver) {
 
         BaseTest.driver = driver;
     }
 
-    public void waitUntilPageIsLoaded() {
-
-        webDriverWait.until(driver -> ((JavascriptExecutor) driver).executeScript("return document.readyState").equals("complete"));
-    }
-
     public static WebDriver getDriver() {
 
         return driver;
-    }
-
-    public void login(String username, String password) {
-
-        Header header = new Header(driver);
-
-        header.clickAccountButton();
-
-        loginPage.setUsername(username);
-        loginPage.setPassword(password);
-        loginPage.clickLoginButton();
-
-    }
-    public LoginPage getLoginPage() {
-
-        return loginPage;
     }
 }
