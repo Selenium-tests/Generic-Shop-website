81d0e73765469f6f6b94bbb373e9ae0ba3503896
aksi1984 <pablo_xmen@op.pl>
2023-11-25 21:08:12 +0100

    Changed the method addToCart() body

--- src/main/java/qa/helpers/ShoppingCartActions.java
+++ src/main/java/qa/helpers/ShoppingCartActions.java
@@ -1,19 +1,25 @@
 package qa.helpers;
 
 import org.openqa.selenium.WebDriver;
-import qa.enums.ProductCategory;
-import qa.pageobject.SiteContentSection;
+import qa.enums.ThumbnailType;
+import qa.pageobject.thumbnails.Thumbnail;
+import qa.enums.ThumbnailCategory;
 import qa.pageobject.header.Header;
 import qa.pageobject.thumbnails.ProductThumbnail;
+import qa.thumbnailgenerators.ThumbnailProvider;
 
 public class ShoppingCartActions {
 
-    public static void addToCart(WebDriver driver, String productName, ProductCategory category) {
+    public static void addToCart(WebDriver driver, ThumbnailCategory category, String productName) {
 
-        SiteContentSection section = new SiteContentSection(driver);
-        ProductThumbnail productThumbnail = section.getProductThumbnail(driver, productName, category);
+        Thumbnail thumbnail = ThumbnailProvider
+                .getFactory(ThumbnailType.PRODUCT)
+                .createThumbnail(driver, category, productName);
 
-        productThumbnail.clickButton();
+        if (thumbnail instanceof ProductThumbnail) {
+
+            ((ProductThumbnail) thumbnail).clickAddToCartButton();
+        }
     }
 
     public static void openCartPage(WebDriver driver) {
