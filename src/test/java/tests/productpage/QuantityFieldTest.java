4e535beb0d97fe74f107590e18ef1347db2d15a3
aksi1984 <pablo_xmen@op.pl>
2023-11-25 21:44:15 +0100

    Added test methods and removed @Parameters

--- src/test/java/tests/productpage/QuantityFieldTest.java
+++ src/test/java/tests/productpage/QuantityFieldTest.java
@@ -2,15 +2,16 @@ package tests.productpage;
 
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 import qa.base.BaseTest;
-import qa.enums.ProductCategory;
-import qa.pageobject.SiteContentSection;
+import qa.enums.ThumbnailCategory;
+import qa.enums.ThumbnailType;
 import qa.pageobject.productpage.ProductPage;
 import qa.pageobject.productpage.QuantityField;
+import qa.pageobject.thumbnails.Thumbnail;
 import qa.provider.MyDataProvider;
 import qa.extentreports.ExtentReportsManager;
+import qa.thumbnailgenerators.ThumbnailProvider;
 
 import java.util.function.Consumer;
 
@@ -25,8 +26,11 @@ public class QuantityFieldTest extends BaseTest {
 
         productName = "Black Top";
 
-        SiteContentSection section = new SiteContentSection(getDriver());
-        section.clickLink(productName, ProductCategory.ALL_BLACK_TOPS);
+        Thumbnail thumbnail = ThumbnailProvider
+                .getFactory(ThumbnailType.PRODUCT)
+                .createThumbnail(getDriver(), ThumbnailCategory.ALL_BLACK_TOPS, productName);
+
+        thumbnail.clickLink();
 
         productPage = new ProductPage(getDriver());
     }
@@ -48,56 +52,60 @@ public class QuantityFieldTest extends BaseTest {
         consumer.accept(productPage.getQuantityField());
     }
 
-    @Parameters({"min"})
-    @Test
+    @Test(dataProvider = "QF_min", dataProviderClass = MyDataProvider.class)
     public void minimumValue(String value) {
 
-        ExtentReportsManager.create("{" + value + "} as the minimum value in the quantity field");
+        ExtentReportsManager.setName("{" + value + "} as the minimum value in the quantity field");
 
         check(value, "View cart\n" + productName + "” has been added to your cart.");
     }
 
-    @Parameters({"aboveMin"})
-    @Test
+    @Test(dataProvider = "QF_aboveMin", dataProviderClass = MyDataProvider.class)
     public void aboveMinimum(String value) {
 
-        ExtentReportsManager.create("{" + value + "} as the min + 1 value in the quantity field");
+        ExtentReportsManager.setName("{" + value + "} as the min + 1 value in the quantity field");
 
         check(value, "View cart\n" + value + " × “" + productName + "” have been added to your cart.");
     }
 
-    @Parameters({"nominal"})
-    @Test
+    @Test(dataProvider = "QF_nominal", dataProviderClass = MyDataProvider.class)
     public void nominal(String value) {
 
-        ExtentReportsManager.create("{" + value + "} as the nominal value in the quantity field");
+        ExtentReportsManager.setName("{" + value + "} as the nominal value in the quantity field");
 
         check(value, "View cart\n"+value + " × “" + productName + "” have been added to your cart.");
     }
 
-    @Parameters({"belowMax"})
-    @Test
+    @Test(dataProvider = "QF_belowMax", dataProviderClass = MyDataProvider.class)
     public void belowMaximum(String value) {
 
-        ExtentReportsManager.create("{" + value + "} as the maximum - 1 value in the quantity field");
+        ExtentReportsManager.setName("{" + value + "} as the maximum - 1 value in the quantity field");
 
-        check(value, "View cart\n"+value + " × “" + productName + "” have been added to your cart.");
+        check(value, "View cart\n "+value + " × “" + productName + "” have been added to your cart.");
     }
 
-    @Parameters({"belowMin"})
-    @Test
-    public void belowMinimum(String value) {
+    @Test(dataProvider = "QF_max", dataProviderClass = MyDataProvider.class)
+    public void maximum(String value) {
 
-        ExtentReportsManager.create("{" + value + "} as the minimum - 1 value in the quantity field");
+        ExtentReportsManager.setName("{" + value + "} as the maximum value in the quantity field");
 
-        check(value, "Cannot add product with \"" + value + "\" quantity");
+        check(value, "View cart\n "+value + " × “" + productName + "” have been added to your cart.");
     }
 
-    @Parameters({"aboveMax"})
-    @Test
+    @Test(dataProvider = "QF_belowZero", dataProviderClass = MyDataProvider.class)
+    public void belowZero(String value) {
+
+        ExtentReportsManager.setName("{" + value + "} as the below zero value in the quantity field");
+
+        int abs = Math.abs(Integer.parseInt(value));
+
+        check(String.valueOf(abs), "View cart\n "+value + " × “" + productName + "” have been added to your cart.");
+    }
+
+    @Test(dataProvider = "QF_aboveMax", dataProviderClass = MyDataProvider.class)
     public void aboveMaximum(String value) {
 
-        ExtentReportsManager.create("{" + value + "} as the maximum + 1 value in the quantity field");
+        ExtentReportsManager.setName("{" + value + "} as the maximum + 1 value in the quantity field");
 
         check(value, "Cannot add product with \"" + value + "\" quantity");
     }
@@ -105,7 +113,7 @@ public class QuantityFieldTest extends BaseTest {
     @Test(dataProvider = "QF_characters1", dataProviderClass = MyDataProvider.class)
     public void specialCharacters1(String value) {
 
-        ExtentReportsManager.create("{" + value + "} as the value in the quantity field");
+        ExtentReportsManager.setName("{" + value + "} as the value in the quantity field");
 
         validationCheck(value,
                 (QuantityField qf)->{
@@ -117,7 +125,7 @@ public class QuantityFieldTest extends BaseTest {
     @Test(dataProvider = "QF_characters2", dataProviderClass = MyDataProvider.class)
     public void specialCharacters2(String value) {
 
-        ExtentReportsManager.create("{" + value + "} as the value in the quantity field");
+        ExtentReportsManager.setName("{" + value + "} as the value in the quantity field");
 
         validationCheck(value,
                 (QuantityField qf)->{
