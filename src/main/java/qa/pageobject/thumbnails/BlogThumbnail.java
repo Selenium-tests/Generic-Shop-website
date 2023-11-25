f43f6fa8097e71767d16945b604c9f1a7f172f58
aksi1984 <pablo_xmen@op.pl>
2023-11-25 22:18:06 +0100

    Removed unnecessary methods

--- src/main/java/qa/pageobject/thumbnails/BlogThumbnail.java
+++ src/main/java/qa/pageobject/thumbnails/BlogThumbnail.java
@@ -1,47 +1,33 @@
 package qa.pageobject.thumbnails;
 
 import qa.base.BasePage;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 
-import java.util.List;
+public class BlogThumbnail extends BasePage implements Thumbnail {
 
-public class BlogThumbnail extends BasePage {
-
-    WebElement child;
     WebElement link;
 
-    String name;
     public BlogThumbnail(WebDriver driver) {
 
         super(driver);
     }
 
-    @FindBy(className = "tyche-recent-post-alternate-widget")
-    List<WebElement> blogs;
+    @Override
+    public void setLink(WebElement link) {
 
-
-    public void setBlog(int index) {
-
-        child = blogs.get(index).findElement(By.className("tyche-post-content"));
-        link = child.findElement(By.xpath(".//a[@href]"));
-        name = link.getText();
+        this.link = link;
     }
 
-    public void clickToLink() {
+    @Override
+    public void clickLink() {
 
         link.click();
     }
 
-    public String getName() {
+    @Override
+    public String getLinkText() {
 
-        return name;
-    }
-
-    public int getSize() {
-
-        return blogs.size();
+        return link.getText();
     }
 }
