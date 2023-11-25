84abc6c20360b4ecb66c0f9d486932183d6a759d
aksi1984 <pablo_xmen@op.pl>
2023-11-25 20:54:23 +0100

    Added test methods

--- src/test/java/tests/shoppingcart/AddingProductToTheCartFromProductPageTest.java
+++ src/test/java/tests/shoppingcart/AddingProductToTheCartFromProductPageTest.java
@@ -4,15 +4,18 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import qa.base.BaseTest;
-import qa.enums.ProductCategory;
-import qa.pageobject.SiteContentSection;
+import qa.enums.ThumbnailCategory;
+import qa.enums.ThumbnailType;
 import qa.pageobject.header.Header;
 import qa.pageobject.productpage.ProductPage;
 import qa.pageobject.shoppingcart.Row;
 import qa.pageobject.shoppingcart.ShoppingCart;
+import qa.pageobject.thumbnails.ProductThumbnail;
+import qa.pageobject.thumbnails.Thumbnail;
 import qa.provider.MyDataProvider;
 import qa.extentreports.ExtentReportsManager;
 import qa.records.Link;
+import qa.thumbnailgenerators.ThumbnailProvider;
 
 
 public class AddingProductToTheCartFromProductPageTest extends BaseTest {
@@ -29,62 +32,85 @@ public class AddingProductToTheCartFromProductPageTest extends BaseTest {
         header = new Header(getDriver());
     }
 
-    private void check(String productName, ProductCategory category) {
+    private void check(String productName, ThumbnailCategory category) {
 
-        SiteContentSection section = new SiteContentSection(getDriver());
-        section.clickLink(productName, category);
+        Thumbnail thumbnail = ThumbnailProvider
+                .getFactory(ThumbnailType.PRODUCT)
+                .createThumbnail(getDriver(), category, productName);
 
-        final String price = productPage.getPrice();
+        if (thumbnail instanceof ProductThumbnail) {
 
-        productPage.getQuantityField().setQuantity("1");
-        productPage.clickAddToCart();
-        header.clickCartButton();
+            String name = thumbnail.getLinkText();
+            String price = ((ProductThumbnail) thumbnail).getPrice();
 
-        Assert.assertFalse(shoppingCart.getContentsLocator().isEmpty(), "The shopping cart is empty");
+            thumbnail.clickLink();
 
-        Row row = shoppingCart.getTable().getRow(0);
+            productPage.getQuantityField().setQuantity("1");
+            productPage.clickAddToCart();
+            header.clickCartButton();
 
-        Assert.assertEquals(row.getName(), productName, "Incorrect product name in the shopping cart");
-        Assert.assertEquals(row.getPrice(), price, "Incorrect price in the shopping cart");
+            Assert.assertFalse(shoppingCart.getContentsLocator().isEmpty(), "The shopping cart is empty");
+
+            Row row = shoppingCart.getTable().getRow(0);
+
+            Assert.assertEquals(row.getName(), name, "Incorrect product name in the shopping cart");
+            Assert.assertEquals(row.getPrice(), price, "Incorrect price in the shopping cart");
+        }
     }
 
     @Test(dataProvider = "allBlackTops", dataProviderClass = MyDataProvider.class)
-    public void fromAllBlackTops(Link link) {
+    public void allBlackTops(Link link) {
 
         ExtentReportsManager.setName("Adding the \"" + link.linkText() + "\" product from the \"ALL BLACK TOPS\" category");
 
-        check(link.linkText(), ProductCategory.ALL_BLACK_TOPS);
+        check(link.linkText(), ThumbnailCategory.ALL_BLACK_TOPS);
     }
 
     @Test(dataProvider = "highHeelShoesProducts", dataProviderClass = MyDataProvider.class)
-    public void fromHighHeelShoes(Link link) {
+    public void highHeelShoes(Link link) {
 
         ExtentReportsManager.setName("Adding the \"" + link.linkText() + "\" product from the \"HIGH HEEL SHOES\" category");
 
-        check(link.linkText(), ProductCategory.HIGH_HEEL_SHOES);
+        check(link.linkText(), ThumbnailCategory.HIGH_HEEL_SHOES);
     }
 
     @Test(dataProvider = "mostWantedProducts", dataProviderClass = MyDataProvider.class)
-    public void fromMostWanted(Link link) {
+    public void mostWanted(Link link) {
 
-        ExtentReportsManager.setName("Adding the \"" + link.linkText() + "\" product from the \"MOST WANTED\" category");
+        //ExtentReportsManager.setName("Adding the \"" + link.linkText() + "\" product from the \"MOST WANTED\" category");
 
-        check(link.linkText(), ProductCategory.MOST_WANTED);
+        check(link.linkText(), ThumbnailCategory.MOST_WANTED);
+    }
+
+    @Test(dataProvider = "scarfsProducts", dataProviderClass = MyDataProvider.class)
+    public void scarfs(Link link) {
+
+        ExtentReportsManager.setName("Adding the \"" + link.linkText() + "\" product from the \"SCARFS\" category");
+
+        check(link.linkText(), ThumbnailCategory.SCARFS);
+    }
+
+    @Test(dataProvider = "onSaleProducts", dataProviderClass = MyDataProvider.class)
+    public void onSale(Link link) {
+
+         ExtentReportsManager.setName("Adding the \"" + link.linkText() + "\" product from the \"ON SALE\" category");
+
+        check(link.linkText(), ThumbnailCategory.ON_SALE);
     }
 
     @Test(dataProvider = "featuredProducts", dataProviderClass = MyDataProvider.class)
-    public void fromFeatured(Link link) {
+    public void featured(Link link) {
 
         ExtentReportsManager.setName("Adding the \"" + link.linkText() + "\" product from the \"FEATURED\" category");
 
-        check(link.linkText(), ProductCategory.FEATURED);
+        check(link.linkText(), ThumbnailCategory.FEATURED);
     }
 
     @Test(dataProvider = "trendsProducts", dataProviderClass = MyDataProvider.class)
-    public void fromTrends(Link link) {
+    public void trends(Link link) {
 
         ExtentReportsManager.setName("Adding the \"" + link.linkText() + "\" product from the \"TRENDS\" category");
 
-        check(link.linkText(), ProductCategory.TRENDS);
+        check(link.linkText(), ThumbnailCategory.TRENDS);
     }
 }
