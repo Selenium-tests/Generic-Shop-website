01b13facce93eacf0ee99b074030832b36b0fb9d
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-08 22:34:29 +0100

    Added test method

--- src/test/java/tests/checkout/PaymentMethodsTest.java
+++ src/test/java/tests/checkout/PaymentMethodsTest.java
@@ -1,10 +1,16 @@
 package tests.checkout;
 
+import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 import qa.base.BaseTest;
 import qa.enums.ThumbnailCategory;
+import qa.helpers.AddressFormFiller;
 import qa.helpers.ShoppingCartActions;
+import qa.pageobject.checkoutpage.CheckoutPage;
 import qa.pageobject.shoppingcart.ShoppingCart;
+import qa.provider.MyDataProvider;
+import qa.records.AddressFormData;
 
 public class PaymentMethodsTest extends BaseTest {
 
@@ -18,5 +24,17 @@ public class PaymentMethodsTest extends BaseTest {
         shoppingCart.clickCheckoutButton();
     }
 
+    @Test(dataProvider = "AF_correctAddress", dataProviderClass = MyDataProvider.class)
+    public void directBankTransfer(AddressFormData data) throws InterruptedException {
 
+        CheckoutPage checkoutPage = new CheckoutPage(getDriver());
+        AddressFormFiller.get(data, getDriver());
+        checkoutPage.getPaymentMethodsSection().clickDirectBankTransferCheckbox();
+        checkoutPage.clickPlaceOrderButton();
+
+        Thread.sleep(3000);
+
+        Assert.assertTrue(getDriver().getCurrentUrl().contains("https://skleptest.pl/checkout/order-received/"),
+                "The page with the address \"https://skleptest.pl/checkout/order-received/\" has not been found");
+    }
 }
