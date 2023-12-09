38fe8af73992f335bd3947dda3fb790f5e87ad17
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-09 07:00:01 +0100

    Added methods

--- src/test/java/tests/checkout/PaymentMethodsTest.java
+++ src/test/java/tests/checkout/PaymentMethodsTest.java
@@ -8,11 +8,16 @@ import qa.enums.ThumbnailCategory;
 import qa.helpers.AddressFormFiller;
 import qa.helpers.ShoppingCartActions;
 import qa.pageobject.checkoutpage.CheckoutPage;
+import qa.pageobject.checkoutpage.PaymentMethodsSection;
 import qa.pageobject.shoppingcart.ShoppingCart;
 import qa.provider.MyDataProvider;
 import qa.records.AddressFormData;
 
+import java.util.function.Consumer;
+
 public class PaymentMethodsTest extends BaseTest {
+
+    private final String url = "https://skleptest.pl/checkout/order-received/";
 
     @BeforeMethod
     public void create() {
@@ -24,17 +29,40 @@ public class PaymentMethodsTest extends BaseTest {
         shoppingCart.clickCheckoutButton();
     }
 
-    @Test(dataProvider = "AF_correctAddress", dataProviderClass = MyDataProvider.class)
-    public void directBankTransfer(AddressFormData data) throws InterruptedException {
+    private void check(AddressFormData data, Consumer<PaymentMethodsSection> consumer) throws InterruptedException {
 
         CheckoutPage checkoutPage = new CheckoutPage(getDriver());
         AddressFormFiller.get(data, getDriver());
-        checkoutPage.getPaymentMethodsSection().clickDirectBankTransferCheckbox();
+        consumer.accept(checkoutPage.getPaymentMethodsSection());
         checkoutPage.clickPlaceOrderButton();
 
         Thread.sleep(3000);
 
         Assert.assertTrue(getDriver().getCurrentUrl().contains("https://skleptest.pl/checkout/order-received/"),
-                "The page with the address \"https://skleptest.pl/checkout/order-received/\" has not been found");
+                "The page with the address \"" + url + "\"has not been found");
+    }
+
+    @Test(dataProvider = "AF_correctAddress", dataProviderClass = MyDataProvider.class)
+    public void directBankTransfer(AddressFormData data) throws InterruptedException {
+
+        check(data, PaymentMethodsSection::clickDirectBankTransferCheckbox);
+    }
+
+    @Test(dataProvider = "AF_correctAddress", dataProviderClass = MyDataProvider.class)
+    public void checkPayments(AddressFormData data) throws InterruptedException {
+
+        check(data, PaymentMethodsSection::clickCheckPaymentsCheckbox);
+    }
+
+    @Test(dataProvider = "AF_correctAddress", dataProviderClass = MyDataProvider.class)
+    public void cashOnDelivery(AddressFormData data) throws InterruptedException {
+
+        check(data, PaymentMethodsSection::clickCashOnDeliveryCheckbox);
+    }
+
+    @Test(dataProvider = "AF_correctAddress", dataProviderClass = MyDataProvider.class)
+    public void payPal(AddressFormData data) throws InterruptedException {
+
+        check(data, PaymentMethodsSection::clickPayPalCheckbox);
     }
 }
