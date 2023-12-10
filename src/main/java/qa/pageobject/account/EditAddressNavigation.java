26b266f3c05278df5d11c12743a0c8ec95b9f664
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-10 23:45:48 +0100

    Added waiting

--- src/main/java/qa/pageobject/account/EditAddressNavigation.java
+++ src/main/java/qa/pageobject/account/EditAddressNavigation.java
@@ -1,14 +1,16 @@
 package qa.pageobject.account;
 
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import qa.base.BasePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class AddressColumns extends BasePage {
 
-    public AddressColumns(WebDriver driver) {
+public class EditAddressNavigation extends BasePage {
+
+    public EditAddressNavigation(WebDriver driver) {
 
         super(driver);
     }
@@ -22,11 +24,11 @@ public class AddressColumns extends BasePage {
 
     public void clickBillingAddressLink() {
 
-        billingAddress.findElement(By.xpath(".//a[@href]")).click();
+        click(By.xpath(".//a[@href]"));
     }
 
     public void clickShippingAddressLink() {
 
-        shippingAddress.findElement(By.xpath(".//a[@href]")).click();
+        getWebDriverWait().until(ExpectedConditions.elementToBeClickable(shippingAddress.findElement(By.xpath(".//a[@href]")))).click();
     }
 }
