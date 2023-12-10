7491d0ec2203f49b22cfd8076b6e2ab11eafa296
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-10 14:13:54 +0100

    Updated @BeforeMethod method

--- src/test/java/tests/footer/FooterNewsletterTests.java
+++ src/test/java/tests/footer/FooterNewsletterTests.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.NoAlertPresentException;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import qa.enums.URLs;
 import qa.pageobject.footer.Footer;
 import qa.provider.MyDataProvider;
 import qa.extentreports.ExtentReportsManager;
@@ -19,12 +20,12 @@ public class FooterNewsletterTests extends BaseTest {
     @BeforeMethod
     public void create() {
 
+        goToSpecificPage(URLs.HOME_PAGE.getName());
         footer = new Footer(getDriver());
     }
 
-    private void fill(Newsletter newsletter) {
+    private void fill(Newsletter newsletter) throws IllegalAccessException {
 
-        footer.getNewsletterForm().clearAll();
         footer.getNewsletterForm().setUsername(newsletter.username());
         footer.getNewsletterForm().setEmail(newsletter.email());
         footer.getNewsletterForm().clickSubscribeButton();
@@ -35,7 +36,6 @@ public class FooterNewsletterTests extends BaseTest {
         try {
 
             Alert alert = getDriver().switchTo().alert();
-
             Assert.assertEquals(alert.getText(), expectedAlertText, message);
 
         } catch (NoAlertPresentException e) {
@@ -45,22 +45,22 @@ public class FooterNewsletterTests extends BaseTest {
     }
 
     @Test(dataProvider = "newsletterCorrectCredentials", dataProviderClass = MyDataProvider.class)
-    public void correctCredentials(Newsletter newsletter) throws InterruptedException {
+    public void correctCredentials(Newsletter newsletter) throws InterruptedException, IllegalAccessException {
 
         ExtentReportsManager.setName("Signing up for the newsletter with correct credentials.");
 
         fill(newsletter);
 
-        Thread.sleep(2000);
+        Thread.sleep(1000);
 
         Assert.assertTrue(footer.getNewsletterForm().isMessageDisplayed(),
                "No message when signing up with correct credentials");
-        Assert.assertEquals(footer.getNewsletterForm().getMessageText(), "Succesfully Subscribed",
+        Assert.assertEquals(footer.getNewsletterForm().getMessageText(), "Successfully Subscribed.",
                 "Incorrect message text when signing up with correct credentials");
     }
 
     @Test(dataProvider = "newsletterBlankUsernameField", dataProviderClass = MyDataProvider.class)
-    public void blankUsernameField(Newsletter newsletter) {
+    public void blankUsernameField(Newsletter newsletter) throws IllegalAccessException {
 
         ExtentReportsManager.setName("Signing up for the newsletter with the blank username field.");
 
@@ -71,7 +71,7 @@ public class FooterNewsletterTests extends BaseTest {
     }
 
     @Test(dataProvider = "newsletterIncorrectEmailFormat", dataProviderClass = MyDataProvider.class)
-    public void incorrectEmailFormat(Newsletter newsletter) {
+    public void incorrectEmailFormat(Newsletter newsletter) throws IllegalAccessException {
 
         ExtentReportsManager.setName("Signing up for the newsletter with an incorrect email format.");
 
@@ -82,7 +82,7 @@ public class FooterNewsletterTests extends BaseTest {
     }
 
     @Test(dataProvider = "newsletterBlankEmailField", dataProviderClass = MyDataProvider.class)
-    public void blankEmailField(Newsletter newsletter) {
+    public void blankEmailField(Newsletter newsletter) throws IllegalAccessException {
 
         ExtentReportsManager.setName("Signing up for the newsletter with the blank email field.");
 
