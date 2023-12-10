f4a1b7e94ffb2f46c26c5e723fc9c0e509566419
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-10 15:02:05 +0100

    Updated

--- src/main/java/qa/helpers/ShoppingCartActions.java
+++ src/main/java/qa/helpers/ShoppingCartActions.java
@@ -1,28 +1,20 @@
 package qa.helpers;
 
 import org.openqa.selenium.WebDriver;
-import qa.enums.ThumbnailType;
-import qa.pageobject.thumbnails.Thumbnail;
-import qa.enums.ThumbnailCategory;
+import qa.pageobject.productpage.ProductPage;
 import qa.pageobject.header.Header;
-import qa.pageobject.thumbnails.ProductThumbnail;
-import qa.thumbnailgenerators.ThumbnailProvider;
+
 
 public class ShoppingCartActions {
 
-    public static void addToCart(WebDriver driver, ThumbnailCategory category, String productName) {
+    public static void addToCart(WebDriver driver) throws IllegalAccessException {
 
-        Thumbnail thumbnail = ThumbnailProvider
-                .getFactory(ThumbnailType.PRODUCT)
-                .createThumbnail(driver, category, productName);
-
-        if (thumbnail instanceof ProductThumbnail) {
-
-            ((ProductThumbnail) thumbnail).clickAddToCartButton();
-        }
+        ProductPage productPage = new ProductPage(driver);
+        productPage.getQuantityField().setQuantity("1");
+        productPage.clickAddToCart();
     }
 
-    public static void openCartPage(WebDriver driver) {
+    public static void openCartPage(WebDriver driver) throws IllegalAccessException {
 
         Header header = new Header(driver);
         header.clickCartButton();
