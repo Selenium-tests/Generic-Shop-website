195b05a4fd74da65a0dedfd01f5334d4eb1a79ad
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-10 09:06:01 +0100

    Added throws IllegalAccessException

--- src/test/java/tests/productpage/QuantityFieldTest.java
+++ src/test/java/tests/productpage/QuantityFieldTest.java
@@ -4,15 +4,10 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import qa.base.BaseTest;
-import qa.enums.ThumbnailCategory;
-import qa.enums.ThumbnailType;
 import qa.pageobject.productpage.ProductPage;
 import qa.pageobject.productpage.QuantityField;
-import qa.pageobject.thumbnails.Thumbnail;
 import qa.provider.MyDataProvider;
 import qa.extentreports.ExtentReportsManager;
-import qa.thumbnailgenerators.ThumbnailProvider;
-
 import java.util.function.Consumer;
 
 
@@ -22,20 +17,14 @@ public class QuantityFieldTest extends BaseTest {
     private String productName;
 
     @BeforeMethod
-    public void create() {
+    public void create() throws IllegalAccessException {
 
+        goToSpecificPage("https://skleptest.pl/product/black-top/");
         productName = "Black Top";
-
-        Thumbnail thumbnail = ThumbnailProvider
-                .getFactory(ThumbnailType.PRODUCT)
-                .createThumbnail(getDriver(), ThumbnailCategory.ALL_BLACK_TOPS, productName);
-
-        thumbnail.clickLink();
-
         productPage = new ProductPage(getDriver());
     }
 
-    private void check(String quantity, String expectedMessageText) {
+    private void check(String quantity, String expectedMessageText) throws IllegalAccessException {
 
         productPage.getQuantityField().setQuantity(quantity);
         productPage.clickAddToCart();
@@ -44,7 +33,7 @@ public class QuantityFieldTest extends BaseTest {
         Assert.assertEquals(productPage.getMessageText(), expectedMessageText, "Incorrect message text");
     }
 
-    private void validationCheck(String quantity, Consumer<QuantityField> consumer) {
+    private void validationCheck(String quantity, Consumer<QuantityField> consumer) throws IllegalAccessException {
 
         productPage.getQuantityField().setQuantity(quantity);
         productPage.clickAddToCart();
@@ -53,49 +42,49 @@ public class QuantityFieldTest extends BaseTest {
     }
 
     @Test(dataProvider = "QF_min", dataProviderClass = MyDataProvider.class)
-    public void minimumValue(String value) {
+    public void minimumValue(String value) throws IllegalAccessException {
 
-        ExtentReportsManager.setName("{" + value + "} as the minimum value in the quantity field");
+        //ExtentReportsManager.setName("{" + value + "} as the minimum value in the quantity field");
 
         check(value, "View cart\n" + productName + "” has been added to your cart.");
     }
 
     @Test(dataProvider = "QF_aboveMin", dataProviderClass = MyDataProvider.class)
-    public void aboveMinimum(String value) {
+    public void aboveMinimum(String value) throws IllegalAccessException {
 
-        ExtentReportsManager.setName("{" + value + "} as the min + 1 value in the quantity field");
+        //ExtentReportsManager.setName("{" + value + "} as the min + 1 value in the quantity field");
 
         check(value, "View cart\n" + value + " × “" + productName + "” have been added to your cart.");
     }
 
     @Test(dataProvider = "QF_nominal", dataProviderClass = MyDataProvider.class)
-    public void nominal(String value) {
+    public void nominal(String value) throws IllegalAccessException {
 
-        ExtentReportsManager.setName("{" + value + "} as the nominal value in the quantity field");
+        //ExtentReportsManager.setName("{" + value + "} as the nominal value in the quantity field");
 
         check(value, "View cart\n"+value + " × “" + productName + "” have been added to your cart.");
     }
 
     @Test(dataProvider = "QF_belowMax", dataProviderClass = MyDataProvider.class)
-    public void belowMaximum(String value) {
+    public void belowMaximum(String value) throws IllegalAccessException {
 
-        ExtentReportsManager.setName("{" + value + "} as the maximum - 1 value in the quantity field");
+        //ExtentReportsManager.setName("{" + value + "} as the maximum - 1 value in the quantity field");
 
         check(value, "View cart\n "+value + " × “" + productName + "” have been added to your cart.");
     }
 
     @Test(dataProvider = "QF_max", dataProviderClass = MyDataProvider.class)
-    public void maximum(String value) {
+    public void maximum(String value) throws IllegalAccessException {
 
-        ExtentReportsManager.setName("{" + value + "} as the maximum value in the quantity field");
+        //ExtentReportsManager.setName("{" + value + "} as the maximum value in the quantity field");
 
         check(value, "View cart\n "+value + " × “" + productName + "” have been added to your cart.");
     }
 
     @Test(dataProvider = "QF_belowZero", dataProviderClass = MyDataProvider.class)
-    public void belowZero(String value) {
+    public void belowZero(String value) throws IllegalAccessException {
 
-        ExtentReportsManager.setName("{" + value + "} as the below zero value in the quantity field");
+       // ExtentReportsManager.setName("{" + value + "} as the below zero value in the quantity field");
 
         int abs = Math.abs(Integer.parseInt(value));
 
@@ -103,7 +92,7 @@ public class QuantityFieldTest extends BaseTest {
     }
 
     @Test(dataProvider = "QF_aboveMax", dataProviderClass = MyDataProvider.class)
-    public void aboveMaximum(String value) {
+    public void aboveMaximum(String value) throws IllegalAccessException {
 
         ExtentReportsManager.setName("{" + value + "} as the maximum + 1 value in the quantity field");
 
@@ -111,7 +100,7 @@ public class QuantityFieldTest extends BaseTest {
     }
 
     @Test(dataProvider = "QF_characters1", dataProviderClass = MyDataProvider.class)
-    public void specialCharacters1(String value) {
+    public void specialCharacters1(String value) throws IllegalAccessException {
 
         ExtentReportsManager.setName("{" + value + "} as the value in the quantity field");
 
@@ -123,7 +112,7 @@ public class QuantityFieldTest extends BaseTest {
     }
 
     @Test(dataProvider = "QF_characters2", dataProviderClass = MyDataProvider.class)
-    public void specialCharacters2(String value) {
+    public void specialCharacters2(String value) throws IllegalAccessException {
 
         ExtentReportsManager.setName("{" + value + "} as the value in the quantity field");
 
