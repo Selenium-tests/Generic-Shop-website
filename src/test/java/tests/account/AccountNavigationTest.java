cff9657b0f74b8ad3e09f29d611406369dad821f
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-10 23:12:30 +0100

    Added throws IllegalAccessException

--- src/test/java/tests/account/AccountNavigationTest.java
+++ src/test/java/tests/account/AccountNavigationTest.java
@@ -5,6 +5,7 @@ import org.json.JSONException;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import qa.enums.URLs;
 import qa.extentreports.ExtentReportsManager;
 import qa.helpers.Authentication;
 import qa.pageobject.account.AccountPage;
@@ -17,15 +18,15 @@ public class AccountNavigationTest extends BaseTest {
     private AccountPage accountPage;
 
     @BeforeMethod
-    private void init() throws JSONException {
+    private void init() throws JSONException, IllegalAccessException {
 
+        goToSpecificPage(URLs.LOGIN_PAGE.getName());
         accountPage = new AccountPage(getDriver());
-
         Authentication.loginWithCredentials(getDriver());
     }
 
     @Test(dataProvider = "accountNavigation", dataProviderClass = MyDataProvider.class)
-    public void link(Link link) {
+    public void link(Link link) throws IllegalAccessException {
 
         ExtentReportsManager.setName("Clicking the \"" + link.linkText() + "\" link");
 
