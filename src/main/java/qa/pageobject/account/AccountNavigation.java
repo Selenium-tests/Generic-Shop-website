014708d7f7187b5ac4a6468724dc5a42abb27e49
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-10 23:13:28 +0100

    Added waiting

--- src/main/java/qa/pageobject/account/AccountNavigation.java
+++ src/main/java/qa/pageobject/account/AccountNavigation.java
@@ -1,10 +1,12 @@
 package qa.pageobject.account;
 
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import qa.base.BasePage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+
 
 public class AccountNavigation extends BasePage {
 
@@ -16,8 +18,8 @@ public class AccountNavigation extends BasePage {
     @FindBy(className = "woocommerce-MyAccount-navigation")
     WebElement navigation;
 
-    public void clickLink(String linkText) {
+    public void clickLink(String linkText) throws IllegalAccessException {
 
-        navigation.findElement(By.linkText(linkText)).click();
+        getWebDriverWait().until(ExpectedConditions.elementToBeClickable(navigation.findElement(By.linkText(linkText)))).click();
     }
 }
