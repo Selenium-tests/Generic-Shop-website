934caae8bb08f8aa943fb5e256cab132f8054aec
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-11 00:01:08 +0100

    Added WebDriverWait and FocusExecutor

--- src/main/java/qa/base/BasePage.java
+++ src/main/java/qa/base/BasePage.java
@@ -1,12 +1,39 @@
 package qa.base;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import qa.tools.jsfocus.FocusExecutor;
+
+import java.time.Duration;
 
 public abstract class BasePage {
+
+        protected final WebDriverWait webDriverWait;
+        private final FocusExecutor focusExecutor;
 
         protected BasePage(WebDriver driver) {
 
                 PageFactory.initElements(driver, this);
+                webDriverWait = new WebDriverWait(driver, Duration.ofSeconds(20));
+                focusExecutor = new FocusExecutor(driver);
+        }
+
+        protected void focus(WebElement element) {
+
+                focusExecutor.perform(element);
+        }
+
+        protected WebDriverWait getWebDriverWait() {
+
+                return webDriverWait;
+        }
+
+        protected void click(By by) {
+
+                webDriverWait.until(ExpectedConditions.elementToBeClickable(by)).click();
         }
 }
