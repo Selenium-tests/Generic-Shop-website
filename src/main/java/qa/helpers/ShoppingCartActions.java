2363ac8a3a21a1e11892dbdda140172d18ce9d51
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-10 23:47:42 +0100

    Added Header

--- src/main/java/qa/helpers/ShoppingCartActions.java
+++ src/main/java/qa/helpers/ShoppingCartActions.java
@@ -7,11 +7,13 @@ import qa.pageobject.header.Header;
 
 public class ShoppingCartActions {
 
-    public static void addToCart(WebDriver driver) throws IllegalAccessException {
+    public static void addToCartAndOpen(WebDriver driver) throws IllegalAccessException {
 
         ProductPage productPage = new ProductPage(driver);
         productPage.getQuantityField().setQuantity("1");
         productPage.clickAddToCart();
+        Header header = new Header(driver);
+        header.clickCartButton();
     }
 
     public static void openCartPage(WebDriver driver) throws IllegalAccessException {
