baaa488d7d2788cf94421adbaf938de791ecdf7c
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-10 23:59:28 +0100

    Created the FocusExecutor

---
+++ src/main/java/qa/tools/jsfocus/FocusExecutor.java
@@ -0,0 +1,22 @@
+package qa.tools.jsfocus;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class FocusExecutor {
+
+    private final JavascriptExecutor executor;
+
+    public FocusExecutor(WebDriver driver) {
+
+        executor = (JavascriptExecutor) driver;
+    }
+
+    public void perform(WebElement element) {
+
+        executor.executeScript("arguments[0].focus();", element);
+    }
+
+
+}
