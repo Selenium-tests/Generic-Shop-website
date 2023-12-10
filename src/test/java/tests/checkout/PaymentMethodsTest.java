e35cec0fd64a75fa6787c7300a3ccb3c5101ebbd
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-10 15:46:38 +0100

    Updated @BeforeMethod method

--- src/test/java/tests/checkout/PaymentMethodsTest.java
+++ src/test/java/tests/checkout/PaymentMethodsTest.java
@@ -4,32 +4,29 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import qa.base.BaseTest;
-import qa.enums.ThumbnailCategory;
+import qa.enums.URLs;
 import qa.helpers.AddressFormFiller;
 import qa.helpers.ShoppingCartActions;
 import qa.pageobject.checkoutpage.CheckoutPage;
 import qa.pageobject.checkoutpage.PaymentMethodsSection;
-import qa.pageobject.shoppingcart.ShoppingCart;
 import qa.provider.MyDataProvider;
 import qa.records.AddressFormData;
+import qa.utils.MyConsumer;
 
-import java.util.function.Consumer;
 
 public class PaymentMethodsTest extends BaseTest {
 
     private final String url = "https://skleptest.pl/checkout/order-received/";
 
     @BeforeMethod
-    public void create() {
+    public void create() throws IllegalAccessException {
 
-        ShoppingCartActions.addToCart(getDriver(), ThumbnailCategory.ALL_BLACK_TOPS, "Black Top");
-        ShoppingCartActions.openCartPage(getDriver());
-
-        ShoppingCart shoppingCart = new ShoppingCart(getDriver());
-        shoppingCart.clickCheckoutButton();
+        goToSpecificPage(URLs.BLACK_TOP_PRODUCT_PAGE.getName());
+        ShoppingCartActions.addToCartAndOpen(getDriver());
+        goToSpecificPage(URLs.CHECKOUT_PAGE.getName());
     }
 
-    private void check(AddressFormData data, Consumer<PaymentMethodsSection> consumer) throws InterruptedException {
+    private void check(AddressFormData data, MyConsumer<PaymentMethodsSection> consumer) throws InterruptedException, IllegalAccessException {
 
         CheckoutPage checkoutPage = new CheckoutPage(getDriver());
         AddressFormFiller.get(data, getDriver());
@@ -43,25 +40,25 @@ public class PaymentMethodsTest extends BaseTest {
     }
 
     @Test(dataProvider = "AF_correctAddress", dataProviderClass = MyDataProvider.class)
-    public void directBankTransfer(AddressFormData data) throws InterruptedException {
+    public void directBankTransfer(AddressFormData data) throws InterruptedException, IllegalAccessException {
 
         check(data, PaymentMethodsSection::clickDirectBankTransferCheckbox);
     }
 
     @Test(dataProvider = "AF_correctAddress", dataProviderClass = MyDataProvider.class)
-    public void checkPayments(AddressFormData data) throws InterruptedException {
+    public void checkPayments(AddressFormData data) throws InterruptedException, IllegalAccessException {
 
         check(data, PaymentMethodsSection::clickCheckPaymentsCheckbox);
     }
 
     @Test(dataProvider = "AF_correctAddress", dataProviderClass = MyDataProvider.class)
-    public void cashOnDelivery(AddressFormData data) throws InterruptedException {
+    public void cashOnDelivery(AddressFormData data) throws InterruptedException, IllegalAccessException {
 
         check(data, PaymentMethodsSection::clickCashOnDeliveryCheckbox);
     }
 
     @Test(dataProvider = "AF_correctAddress", dataProviderClass = MyDataProvider.class)
-    public void payPal(AddressFormData data) throws InterruptedException {
+    public void payPal(AddressFormData data) throws InterruptedException, IllegalAccessException {
 
         check(data, PaymentMethodsSection::clickPayPalCheckbox);
     }
