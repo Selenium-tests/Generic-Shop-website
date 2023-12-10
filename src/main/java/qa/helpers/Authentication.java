f000634214fb67f9c320b0d311438ff913ccc003
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-10 23:15:29 +0100

    Removed Header

--- src/main/java/qa/helpers/Authentication.java
+++ src/main/java/qa/helpers/Authentication.java
@@ -2,16 +2,13 @@ package qa.helpers;
 
 import org.openqa.selenium.WebDriver;
 import qa.pageobject.LoginPage;
-import qa.pageobject.header.Header;
+
 
 public class Authentication {
 
-    public static void loginWithCredentials(WebDriver driver) {
+    public static void loginWithCredentials(WebDriver driver) throws IllegalAccessException {
 
-        Header header = new Header(driver);
         LoginPage loginPage = new LoginPage(driver);
-
-        header.clickAccountButton();
         loginPage.setUsername("karen@gmail.com");
         loginPage.setPassword("Kvc$11324#");
         loginPage.clickLoginButton();
