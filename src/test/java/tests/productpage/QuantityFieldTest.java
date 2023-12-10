7bdee5054965e262101c105e28afbdbc677c998d
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-10 10:51:37 +0100

    Changed the @BeforeMethod method body

--- src/test/java/tests/productpage/QuantityFieldTest.java
+++ src/test/java/tests/productpage/QuantityFieldTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import qa.base.BaseTest;
+import qa.enums.URLs;
 import qa.pageobject.productpage.ProductPage;
 import qa.pageobject.productpage.QuantityField;
 import qa.provider.MyDataProvider;
@@ -19,7 +20,7 @@ public class QuantityFieldTest extends BaseTest {
     @BeforeMethod
     public void create() throws IllegalAccessException {
 
-        goToSpecificPage("https://skleptest.pl/product/black-top/");
+        goToSpecificPage(URLs.BLACK_TOP_PRODUCT_PAGE.getName());
         productName = "Black Top";
         productPage = new ProductPage(getDriver());
     }
@@ -44,7 +45,7 @@ public class QuantityFieldTest extends BaseTest {
     @Test(dataProvider = "QF_min", dataProviderClass = MyDataProvider.class)
     public void minimumValue(String value) throws IllegalAccessException {
 
-        //ExtentReportsManager.setName("{" + value + "} as the minimum value in the quantity field");
+        ExtentReportsManager.setName("{" + value + "} as the minimum value in the quantity field");
 
         check(value, "View cart\n" + productName + "” has been added to your cart.");
     }
@@ -52,7 +53,7 @@ public class QuantityFieldTest extends BaseTest {
     @Test(dataProvider = "QF_aboveMin", dataProviderClass = MyDataProvider.class)
     public void aboveMinimum(String value) throws IllegalAccessException {
 
-        //ExtentReportsManager.setName("{" + value + "} as the min + 1 value in the quantity field");
+        ExtentReportsManager.setName("{" + value + "} as the min + 1 value in the quantity field");
 
         check(value, "View cart\n" + value + " × “" + productName + "” have been added to your cart.");
     }
@@ -60,7 +61,7 @@ public class QuantityFieldTest extends BaseTest {
     @Test(dataProvider = "QF_nominal", dataProviderClass = MyDataProvider.class)
     public void nominal(String value) throws IllegalAccessException {
 
-        //ExtentReportsManager.setName("{" + value + "} as the nominal value in the quantity field");
+        ExtentReportsManager.setName("{" + value + "} as the nominal value in the quantity field");
 
         check(value, "View cart\n"+value + " × “" + productName + "” have been added to your cart.");
     }
@@ -68,7 +69,7 @@ public class QuantityFieldTest extends BaseTest {
     @Test(dataProvider = "QF_belowMax", dataProviderClass = MyDataProvider.class)
     public void belowMaximum(String value) throws IllegalAccessException {
 
-        //ExtentReportsManager.setName("{" + value + "} as the maximum - 1 value in the quantity field");
+        ExtentReportsManager.setName("{" + value + "} as the maximum - 1 value in the quantity field");
 
         check(value, "View cart\n "+value + " × “" + productName + "” have been added to your cart.");
     }
@@ -76,7 +77,7 @@ public class QuantityFieldTest extends BaseTest {
     @Test(dataProvider = "QF_max", dataProviderClass = MyDataProvider.class)
     public void maximum(String value) throws IllegalAccessException {
 
-        //ExtentReportsManager.setName("{" + value + "} as the maximum value in the quantity field");
+        ExtentReportsManager.setName("{" + value + "} as the maximum value in the quantity field");
 
         check(value, "View cart\n "+value + " × “" + productName + "” have been added to your cart.");
     }
@@ -84,7 +85,7 @@ public class QuantityFieldTest extends BaseTest {
     @Test(dataProvider = "QF_belowZero", dataProviderClass = MyDataProvider.class)
     public void belowZero(String value) throws IllegalAccessException {
 
-       // ExtentReportsManager.setName("{" + value + "} as the below zero value in the quantity field");
+        ExtentReportsManager.setName("{" + value + "} as the below zero value in the quantity field");
 
         int abs = Math.abs(Integer.parseInt(value));
 
