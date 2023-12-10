cccb2c102c6e35ef9b9daf02604ca0f16d74760d
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-10 13:01:27 +0100

    Changed the @BeforeMethod method body

--- src/test/java/tests/homepage/MainMenuTest.java
+++ src/test/java/tests/homepage/MainMenuTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import qa.base.BaseTest;
+import qa.enums.URLs;
 import qa.extentreports.ExtentReportsManager;
 import qa.pageobject.mainmenu.MainMenu;
 import qa.provider.MyDataProvider;
@@ -16,11 +17,12 @@ public class MainMenuTest extends BaseTest {
     @BeforeMethod
     public void create() {
 
+        goToSpecificPage(URLs.HOME_PAGE.getName());
         mainMenu = new MainMenu(getDriver());
     }
 
     @Test(dataProvider = "mainMenu", dataProviderClass = MyDataProvider.class)
-    public void mainMenu(Link link) {
+    public void mainMenu(Link link) throws IllegalAccessException {
 
         ExtentReportsManager.setName("Clicking the \"" + link.linkText() + "\" link in the main menu");
 
