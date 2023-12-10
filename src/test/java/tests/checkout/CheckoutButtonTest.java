236c290c428b0922bef82fede9ef321e52308c9a
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-10 14:47:32 +0100

    Created the CheckoutButtonTest

---
+++ src/test/java/tests/checkout/CheckoutButtonTest.java
@@ -0,0 +1,35 @@
+package tests.checkout;
+
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import qa.base.BaseTest;
+import qa.enums.URLs;
+import qa.extentreports.ExtentReportsManager;
+import qa.helpers.ShoppingCartActions;
+import qa.pageobject.shoppingcart.ShoppingCart;
+import qa.provider.MyDataProvider;
+import qa.records.Link;
+
+public class CheckoutButtonTest extends BaseTest {
+
+    @BeforeMethod
+    public void create() throws IllegalAccessException {
+
+        goToSpecificPage(URLs.BLACK_TOP_PRODUCT_PAGE.getName());
+        ShoppingCartActions.addToCart(getDriver());
+        ShoppingCartActions.openCartPage(getDriver());
+    }
+
+    @Test(dataProvider = "checkoutPage", dataProviderClass = MyDataProvider.class)
+    public void clickingCheckoutButton(Link link) throws IllegalAccessException {
+
+        ExtentReportsManager.setName("clicking the \"Proceed to checkout\" button");
+
+        ShoppingCart shoppingCart = new ShoppingCart(getDriver());
+        shoppingCart.clickCheckoutButton();
+
+        Assert.assertEquals(getDriver().getCurrentUrl(), link.pageURL(),
+                "The page with the address \"" + link.pageURL() + "\" has not been found");
+    }
+}
