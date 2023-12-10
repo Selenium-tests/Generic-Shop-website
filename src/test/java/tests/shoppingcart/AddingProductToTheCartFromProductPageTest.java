55d9e51dd431c5a6684e5a3cd4084d56cc79997f
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-10 10:41:33 +0100

    Changed the check() method body

--- src/test/java/tests/shoppingcart/AddingProductToTheCartFromProductPageTest.java
+++ src/test/java/tests/shoppingcart/AddingProductToTheCartFromProductPageTest.java
@@ -4,18 +4,13 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import qa.base.BaseTest;
-import qa.enums.ThumbnailCategory;
-import qa.enums.ThumbnailType;
 import qa.pageobject.header.Header;
 import qa.pageobject.productpage.ProductPage;
 import qa.pageobject.shoppingcart.Row;
 import qa.pageobject.shoppingcart.ShoppingCart;
-import qa.pageobject.thumbnails.ProductThumbnail;
-import qa.pageobject.thumbnails.Thumbnail;
 import qa.provider.MyDataProvider;
 import qa.extentreports.ExtentReportsManager;
 import qa.records.Link;
-import qa.thumbnailgenerators.ThumbnailProvider;
 
 
 public class AddingProductToTheCartFromProductPageTest extends BaseTest {
@@ -32,85 +27,83 @@ public class AddingProductToTheCartFromProductPageTest extends BaseTest {
         header = new Header(getDriver());
     }
 
-    private void check(String productName, ThumbnailCategory category) {
+    private void check() throws IllegalAccessException {
 
-        Thumbnail thumbnail = ThumbnailProvider
-                .getFactory(ThumbnailType.PRODUCT)
-                .createThumbnail(getDriver(), category, productName);
+        String name = productPage.getProductTitle();
+        String price = productPage.getPrice();
 
-        if (thumbnail instanceof ProductThumbnail) {
+        productPage.getQuantityField().setQuantity("1");
+        productPage.clickAddToCart();
+        header.clickCartButton();
 
-            String name = thumbnail.getLinkText();
-            String price = ((ProductThumbnail) thumbnail).getPrice();
+        Assert.assertFalse(shoppingCart.getContentsLocator().isEmpty(), "The shopping cart is empty");
 
-            thumbnail.clickLink();
+        Row row = shoppingCart.getTable().getRow(0);
 
-            productPage.getQuantityField().setQuantity("1");
-            productPage.clickAddToCart();
-            header.clickCartButton();
-
-            Assert.assertFalse(shoppingCart.getContentsLocator().isEmpty(), "The shopping cart is empty");
-
-            Row row = shoppingCart.getTable().getRow(0);
-
-            Assert.assertEquals(row.getName(), name, "Incorrect product name in the shopping cart");
-            Assert.assertEquals(row.getPrice(), price, "Incorrect price in the shopping cart");
-        }
+        Assert.assertEquals(row.getName(), name, "Incorrect product name in the shopping cart");
+        Assert.assertEquals(row.getPrice(), price, "Incorrect price in the shopping cart");
     }
 
     @Test(dataProvider = "allBlackTops", dataProviderClass = MyDataProvider.class)
-    public void allBlackTops(Link link) {
+    public void allBlackTops(Link link) throws IllegalAccessException {
 
         ExtentReportsManager.setName("Adding the \"" + link.linkText() + "\" product from the \"ALL BLACK TOPS\" category");
 
-        check(link.linkText(), ThumbnailCategory.ALL_BLACK_TOPS);
+        goToSpecificPage(link.pageURL());
+        check();
     }
 
     @Test(dataProvider = "highHeelShoesProducts", dataProviderClass = MyDataProvider.class)
-    public void highHeelShoes(Link link) {
+    public void highHeelShoes(Link link) throws IllegalAccessException {
 
         ExtentReportsManager.setName("Adding the \"" + link.linkText() + "\" product from the \"HIGH HEEL SHOES\" category");
 
-        check(link.linkText(), ThumbnailCategory.HIGH_HEEL_SHOES);
+        goToSpecificPage(link.pageURL());
+        check();
     }
 
     @Test(dataProvider = "mostWantedProducts", dataProviderClass = MyDataProvider.class)
-    public void mostWanted(Link link) {
+    public void mostWanted(Link link) throws IllegalAccessException {
 
-        //ExtentReportsManager.setName("Adding the \"" + link.linkText() + "\" product from the \"MOST WANTED\" category");
+        ExtentReportsManager.setName("Adding the \"" + link.linkText() + "\" product from the \"MOST WANTED\" category");
 
-        check(link.linkText(), ThumbnailCategory.MOST_WANTED);
+        goToSpecificPage(link.pageURL());
+        check();
     }
 
     @Test(dataProvider = "scarfsProducts", dataProviderClass = MyDataProvider.class)
-    public void scarfs(Link link) {
+    public void scarfs(Link link) throws IllegalAccessException {
 
         ExtentReportsManager.setName("Adding the \"" + link.linkText() + "\" product from the \"SCARFS\" category");
 
-        check(link.linkText(), ThumbnailCategory.SCARFS);
+        goToSpecificPage(link.pageURL());
+        check();
     }
 
     @Test(dataProvider = "onSaleProducts", dataProviderClass = MyDataProvider.class)
-    public void onSale(Link link) {
+    public void onSale(Link link) throws IllegalAccessException {
 
-         ExtentReportsManager.setName("Adding the \"" + link.linkText() + "\" product from the \"ON SALE\" category");
+        ExtentReportsManager.setName("Adding the \"" + link.linkText() + "\" product from the \"ON SALE\" category");
 
-        check(link.linkText(), ThumbnailCategory.ON_SALE);
+        goToSpecificPage(link.pageURL());
+        check();
     }
 
     @Test(dataProvider = "featuredProducts", dataProviderClass = MyDataProvider.class)
-    public void featured(Link link) {
+    public void featured(Link link) throws IllegalAccessException {
 
         ExtentReportsManager.setName("Adding the \"" + link.linkText() + "\" product from the \"FEATURED\" category");
 
-        check(link.linkText(), ThumbnailCategory.FEATURED);
+        goToSpecificPage(link.pageURL());
+        check();
     }
 
     @Test(dataProvider = "trendsProducts", dataProviderClass = MyDataProvider.class)
-    public void trends(Link link) {
+    public void trends(Link link) throws IllegalAccessException {
 
         ExtentReportsManager.setName("Adding the \"" + link.linkText() + "\" product from the \"TRENDS\" category");
 
-        check(link.linkText(), ThumbnailCategory.TRENDS);
+        goToSpecificPage(link.pageURL());
+        check();
     }
 }
