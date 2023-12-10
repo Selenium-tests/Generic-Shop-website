bf93b149304924b2c49a85428813a5339fb1819d
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-10 12:54:15 +0100

    Changed the @BeforeMethod method body

--- src/test/java/tests/homepage/MainMenuDropdownListTest.java
+++ src/test/java/tests/homepage/MainMenuDropdownListTest.java
@@ -4,6 +4,7 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import qa.base.BaseTest;
+import qa.enums.URLs;
 import qa.extentreports.ExtentReportsManager;
 import qa.pageobject.mainmenu.DropdownList;
 import qa.provider.MyDataProvider;
@@ -16,11 +17,12 @@ public class MainMenuDropdownListTest extends BaseTest {
     @BeforeMethod
     public void create() {
 
+        goToSpecificPage(URLs.HOME_PAGE.getName());
         dropdownList = new DropdownList(getDriver());
     }
 
     @Test(dataProvider = "mainMenuDropdownList", dataProviderClass = MyDataProvider.class)
-    void link(Link link) {
+    void link(Link link) throws IllegalAccessException {
 
         ExtentReportsManager.setName("Clicking the \"" + link + "\" item in the main menu drop-down list");
 
