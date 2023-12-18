3ecb740c3a7b0bebbe017d94a01b56fdfd796ed0
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2023-12-18 18:11:28 +0100

    Removed Capabilities and System.ou.println

--- src/main/java/qa/browsermanager/BrowserManager.java
+++ src/main/java/qa/browsermanager/BrowserManager.java
@@ -8,22 +8,13 @@ import java.time.Duration;
 
 public class BrowserManager {
 
-    public static void openBrowser(WebDriver driver, String url) {
+    public static void goToPage(WebDriver driver, String url) {
 
-        Capabilities capabilities = ((RemoteWebDriver) driver).getCapabilities();
-
-        System.out.println("The " + capabilities.getBrowserName() + " " + capabilities.getBrowserVersion() + " is running...");
-
-        driver.navigate().to(url);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.get(url);
         driver.manage().window().maximize();
     }
 
     public static void closeBrowser(WebDriver driver) {
-
-        Capabilities capabilities = ((RemoteWebDriver) driver).getCapabilities();
-
-        System.out.println("The " + capabilities.getBrowserName() + " " + capabilities.getBrowserVersion() + " is closing");
 
         driver.quit();
     }
