c22accddc640f9949df8318a79b346c9ac493f0a
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-14 11:05:50 +0100

    Added test methods

--- src/test/java/tests/checkout/FunctionalitiesTest.java
+++ src/test/java/tests/checkout/FunctionalitiesTest.java
@@ -5,12 +5,14 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import qa.base.BaseTest;
 import qa.enums.URLs;
-import qa.extentreports.ExtentReportsManager;
 import qa.helpers.ShoppingCartActions;
 import qa.pageobject.checkoutpage.CheckoutPage;
+import qa.provider.MyDataProvider;
 
 
 public class FunctionalitiesTest extends BaseTest {
+
+    private CheckoutPage checkoutPage;
 
     @BeforeMethod
     public void create() throws IllegalAccessException {
@@ -18,17 +20,51 @@ public class FunctionalitiesTest extends BaseTest {
         goToSpecificPage(URLs.BLACK_TOP_PRODUCT_PAGE.getName());
         ShoppingCartActions.addToCartAndOpen(getDriver());
         goToSpecificPage(URLs.CHECKOUT_PAGE.getName());
+        checkoutPage = new CheckoutPage(getDriver());
     }
 
     @Test
-    public void makingShippingAddressFormVisible() throws IllegalAccessException {
+    public void shippingAddressFormVisibility() throws IllegalAccessException {
 
-        ExtentReportsManager.setName("Clicking the checkbox above the shipping address form");
-
-        CheckoutPage checkoutPage = new CheckoutPage(getDriver());
         checkoutPage.clickDifferentAddressCheckbox();
 
-        Assert.assertTrue(checkoutPage.getShippingAddressForm().isVisible(),
-                "The shipping address form is not visible");
+        try {
+            checkoutPage.getShippingAddressForm().waitForContentsLocatorValidAttribute();
+        } catch (Exception e) {
+            Assert.fail("The shipping address form is not displayed");
+        }
+    }
+
+    @Test
+    public void loginFormVisibility() {
+
+        checkoutPage.clickLoginLink();
+
+        try {
+            checkoutPage.getLoginForm().waitForContentsLocatorValidAttribute();
+        } catch (Exception e) {
+            Assert.fail("The login form is not displayed");
+        }
+    }
+
+    @Test
+    public void couponCodeFormVisibility() throws IllegalAccessException {
+
+        checkoutPage.clickCouponCodeLink();
+
+        try {
+            checkoutPage.getCouponCodeForm().waitForContentsLocatorValidAttribute();
+        } catch (Exception e) {
+            Assert.fail("The coupon code form is not displayed");
+        }
+    }
+
+    @Test(dataProvider = "orderComments", dataProviderClass = MyDataProvider.class)
+    public void orderCommentsField(String comment) throws IllegalAccessException {
+
+        checkoutPage.getOrderCommentsField().setComment(comment);
+
+        Assert.assertEquals(checkoutPage.getOrderCommentsField().getComment(), comment,
+                "Incorrect order comment");
     }
 }
