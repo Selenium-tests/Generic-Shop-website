4f68720e6421304dc7d3d0a3345ac97637b60664
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-14 23:52:32 +0100

    Added SHOPPING_CART

--- src/main/java/qa/enums/URLs.java
+++ src/main/java/qa/enums/URLs.java
@@ -5,6 +5,7 @@ public enum URLs {
     HOME_PAGE("http://skleptest.pl/"),
     LOGIN_PAGE("https://skleptest.pl/my-account/"),
     BLACK_TOP_PRODUCT_PAGE("https://skleptest.pl/product/black-top/"),
+    SHOPPING_CART("https://skleptest.pl/cart/"),
     CHECKOUT_PAGE("https://skleptest.pl/checkout/"),
     BILLING_ADDRESS_FORM("https://skleptest.pl/my-account/edit-address/billing/"),
     EDIT_ADDRESS_NAVIGATION("https://skleptest.pl/my-account/edit-address/"),
