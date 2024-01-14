e6fae1db8d04e41a22109910379e614a9db6f29b
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-14 23:44:59 +0100

    Removed the ExtentReportsManager.setName

--- src/test/java/tests/checkout/CheckoutButtonTest.java
+++ src/test/java/tests/checkout/CheckoutButtonTest.java
@@ -5,10 +5,9 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import qa.base.BaseTest;
 import qa.enums.URLs;
-import qa.extentreports.ExtentReportsManager;
 import qa.helpers.ShoppingCartActions;
 import qa.pageobject.shoppingcart.ShoppingCart;
-import qa.provider.MyDataProvider;
+import qa.dataproviders.DataProviders;
 import qa.data.Link;
 
 public class CheckoutButtonTest extends BaseTest {
@@ -18,12 +17,11 @@ public class CheckoutButtonTest extends BaseTest {
 
         goToSpecificPage(URLs.BLACK_TOP_PRODUCT_PAGE.getName());
         ShoppingCartActions.addToCartAndOpen(getDriver());
+        goToSpecificPage(URLs.SHOPPING_CART.getName());
     }
 
-    @Test(dataProvider = "checkoutPage", dataProviderClass = MyDataProvider.class)
+    @Test(dataProvider = "checkoutPage", dataProviderClass = DataProviders.class)
     public void clickingCheckoutButton(Link link) throws IllegalAccessException {
-
-        ExtentReportsManager.setName("clicking the \"Proceed to checkout\" button");
 
         ShoppingCart shoppingCart = new ShoppingCart(getDriver());
         shoppingCart.clickCheckoutButton();
