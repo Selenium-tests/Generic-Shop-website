b3f9f4d4f4ec0aa6abe4e122a30310950c907b82
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-15 00:25:37 +0100

    Removed the method

--- src/main/java/qa/helpers/ShoppingCartActions.java
+++ src/main/java/qa/helpers/ShoppingCartActions.java
@@ -2,23 +2,14 @@ package qa.helpers;
 
 import org.openqa.selenium.WebDriver;
 import qa.pageobject.productpage.ProductPage;
-import qa.pageobject.header.Header;
 
 
 public class ShoppingCartActions {
 
-    public static void addToCartAndOpen(WebDriver driver) throws IllegalAccessException {
+    public static void addToCart(WebDriver driver) throws IllegalAccessException {
 
         ProductPage productPage = new ProductPage(driver);
         productPage.getQuantityField().setQuantity("1");
         productPage.clickAddToCart();
-        Header header = new Header(driver);
-        header.clickCartButton();
-    }
-
-    public static void openCartPage(WebDriver driver) throws IllegalAccessException {
-
-        Header header = new Header(driver);
-        header.clickCartButton();
     }
 }
