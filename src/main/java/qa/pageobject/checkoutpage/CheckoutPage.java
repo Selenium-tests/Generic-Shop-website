ecf74a3cd907863d82d2b357c3e0982873b7e480
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-14 11:04:15 +0100

    Added locators and methods

--- src/main/java/qa/pageobject/checkoutpage/CheckoutPage.java
+++ src/main/java/qa/pageobject/checkoutpage/CheckoutPage.java
@@ -5,30 +5,51 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import qa.base.BasePage;
-import qa.pageobject.addressform.AddressForm;
+import qa.pageobject.LoginForm;
 import qa.pageobject.addressform.ShippingAddressForm;
+import qa.pageobject.shoppingcart.CouponCodeForm;
 
 
 public class CheckoutPage extends BasePage {
 
-    private final AddressForm addressForm;
+    private final LoginForm loginForm;
+    private final CouponCodeForm couponCodeForm;
     private final ShippingAddressForm shippingAddressForm;
+    private final OrderCommentsField orderCommentsField;
     private final PaymentMethodsSection paymentMethodsSection;
 
     public CheckoutPage(WebDriver driver) {
 
         super(driver);
 
-        addressForm = new AddressForm(driver);
+        loginForm = new LoginForm(driver);
+        couponCodeForm = new CouponCodeForm(driver);
         shippingAddressForm = new ShippingAddressForm(driver);
+        orderCommentsField = new OrderCommentsField(driver);
         paymentMethodsSection = new PaymentMethodsSection(driver);
     }
+
+    @FindBy(className = "showlogin")
+    WebElement loginLink;
+
+    @FindBy(className = "showcoupon")
+    WebElement couponCodeLink;
 
     @FindBy(id = "ship-to-different-address-checkbox")
     WebElement differentAddressCheckbox;
 
     @FindBy(id = "place_order")
     WebElement placeOrderButton;
+
+    public void clickLoginLink() {
+
+        getWebDriverWait().until(ExpectedConditions.elementToBeClickable(loginLink)).click();
+    }
+
+    public void clickCouponCodeLink() {
+
+        getWebDriverWait().until(ExpectedConditions.elementToBeClickable(couponCodeLink)).click();
+    }
 
     public void clickDifferentAddressCheckbox() {
 
@@ -40,14 +61,24 @@ public class CheckoutPage extends BasePage {
         getWebDriverWait().until(ExpectedConditions.elementToBeClickable(placeOrderButton)).click();
     }
 
-    public AddressForm getAddressForm() {
+    public LoginForm getLoginForm() {
 
-        return addressForm;
+        return loginForm;
+    }
+
+    public CouponCodeForm getCouponCodeForm() {
+
+        return couponCodeForm;
     }
 
     public ShippingAddressForm getShippingAddressForm() {
 
         return shippingAddressForm;
+    }
+
+    public OrderCommentsField getOrderCommentsField() {
+
+        return orderCommentsField;
     }
 
     public PaymentMethodsSection getPaymentMethodsSection() {
