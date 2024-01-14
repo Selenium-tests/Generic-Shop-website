b9fff0ab00dad6a53656650eb0db11a31c66bc53
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-15 00:24:07 +0100

    Added methods

--- src/test/java/tests/checkout/PaymentMethodsTest.java
+++ src/test/java/tests/checkout/PaymentMethodsTest.java
@@ -5,68 +5,75 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import qa.base.BaseTest;
 import qa.enums.URLs;
-import qa.extentreports.ExtentReportsManager;
 import qa.helpers.AddressFormFiller;
 import qa.helpers.ShoppingCartActions;
 import qa.pageobject.checkoutpage.CheckoutPage;
 import qa.pageobject.checkoutpage.PaymentMethodsSection;
-import qa.provider.MyDataProvider;
+import qa.dataproviders.DataProviders;
 import qa.data.AddressFormData;
-import qa.utils.MyConsumer;
+import qa.pageobject.orderreceivedpage.OrderPage;
+import qa.utils.AccessThrowingConsumer;
 
 
 public class PaymentMethodsTest extends BaseTest {
+
+    private CheckoutPage checkoutPage;
 
     @BeforeMethod
     public void create() throws IllegalAccessException {
 
         goToSpecificPage(URLs.BLACK_TOP_PRODUCT_PAGE.getName());
-        ShoppingCartActions.addToCartAndOpen(getDriver());
+        ShoppingCartActions.addToCart(getDriver());
         goToSpecificPage(URLs.CHECKOUT_PAGE.getName());
+
+        checkoutPage = new CheckoutPage(getDriver());
+
+        AddressFormData addressFormData = (AddressFormData) new DataProviders().AF_correctAddress()[0];
+        AddressFormFiller.get(addressFormData, getDriver(), true);
     }
 
-    private void check(AddressFormData data, MyConsumer<PaymentMethodsSection> consumer) throws InterruptedException, IllegalAccessException {
+    public void waitForOrderPage(URLs url) {
 
-        CheckoutPage checkoutPage = new CheckoutPage(getDriver());
-        AddressFormFiller.get(data, getDriver(), true);
+        OrderPage orderPage = new OrderPage(getDriver());
+
+        try {
+            orderPage.waitUntilThePageIsOpen(url.getName());
+        } catch (Exception e) {
+            Assert.fail("The \"" + url.getName() + "\" received page is not open");
+        }
+    }
+
+    private void check(AccessThrowingConsumer<PaymentMethodsSection> consumer) throws IllegalAccessException {
+
         consumer.accept(checkoutPage.getPaymentMethodsSection());
         checkoutPage.clickPlaceOrderButton();
-
-        Thread.sleep(3000);
-
-        Assert.assertTrue(getDriver().getCurrentUrl().contains(URLs.ORDER_RECEIVED.getName()),
-                "The page with the address \"" + URLs.ORDER_RECEIVED.getName() + "\"has not been found");
     }
 
-    @Test(dataProvider = "AF_correctAddress", dataProviderClass = MyDataProvider.class)
-    public void directBankTransfer(AddressFormData data) throws InterruptedException, IllegalAccessException {
+    @Test
+    public void directBankTransfer() throws IllegalAccessException {
 
-        ExtentReportsManager.setName("Payment via direct bank transfer");
-
-        check(data, PaymentMethodsSection::clickDirectBankTransferCheckbox);
+        check(PaymentMethodsSection::clickDirectBankTransferCheckbox);
+        waitForOrderPage(URLs.ORDER_RECEIVED);
     }
 
-    @Test(dataProvider = "AF_correctAddress", dataProviderClass = MyDataProvider.class)
-    public void checkPayments(AddressFormData data) throws InterruptedException, IllegalAccessException {
+    @Test
+    public void checkPayments() throws IllegalAccessException {
 
-        ExtentReportsManager.setName("Payment via check payments");
-
-        check(data, PaymentMethodsSection::clickCheckPaymentsCheckbox);
+        check(PaymentMethodsSection::clickCheckPaymentsCheckbox);
+        waitForOrderPage(URLs.ORDER_RECEIVED);
     }
 
-    @Test(dataProvider = "AF_correctAddress", dataProviderClass = MyDataProvider.class)
-    public void cashOnDelivery(AddressFormData data) throws InterruptedException, IllegalAccessException {
+    @Test
+    public void cashOnDelivery() throws IllegalAccessException {
 
-        ExtentReportsManager.setName("Payment via cash on delivery");
-
-        check(data, PaymentMethodsSection::clickCashOnDeliveryCheckbox);
+        check(PaymentMethodsSection::clickCashOnDeliveryCheckbox);
+        waitForOrderPage(URLs.ORDER_RECEIVED);
     }
 
-    @Test(dataProvider = "AF_correctAddress", dataProviderClass = MyDataProvider.class)
-    public void payPal(AddressFormData data) throws InterruptedException, IllegalAccessException {
+    @Test()
+    public void payPal() throws IllegalAccessException {
 
-        ExtentReportsManager.setName("Payment via PayPal");
-
-        check(data, PaymentMethodsSection::clickPayPalCheckbox);
+        check(PaymentMethodsSection::clickPayPalCheckbox);
+        waitForOrderPage(URLs.PAY_PAL);
     }
 }
