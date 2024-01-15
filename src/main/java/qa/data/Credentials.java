58ebf606a742f780157072d35e22f1d441f4302e
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-15 18:29:21 +0100

    Added field

--- src/main/java/qa/data/Credentials.java
+++ src/main/java/qa/data/Credentials.java
@@ -8,10 +8,12 @@ public class Credentials {
 
     private final String email;
     private final String password;
+    private final String errorMessage;
 
-    public Credentials(String email, String password) {
+    public Credentials(String email, String password, String errorMessage) {
 
         this.email = email;
         this.password = password;
+        this.errorMessage = errorMessage;
     }
 }
