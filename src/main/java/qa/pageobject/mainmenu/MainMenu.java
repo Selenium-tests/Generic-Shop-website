591717b64a1dd83366f62d0533bf295330c8ee8d
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-15 09:44:23 +0100

    Removed the throws IllegalAccessException

--- src/main/java/qa/pageobject/mainmenu/MainMenu.java
+++ src/main/java/qa/pageobject/mainmenu/MainMenu.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import qa.base.BasePage;
 
 public class MainMenu extends BasePage {
@@ -16,8 +17,8 @@ public class MainMenu extends BasePage {
     @FindBy(id = "desktop-menu")
     WebElement siteNavigation;
 
-    public void clickLink(String id) throws IllegalAccessException {
+    public void clickLink(String id) {
 
-        siteNavigation.findElement(By.id(id)).click();
+        getWebDriverWait().until(ExpectedConditions.elementToBeClickable(siteNavigation.findElement(By.id(id)))).click();
     }
 }
