a5f488a06b4448d404a7d13d5b70c11343cdad34
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-17 09:02:35 +0100

    Removed waiting

--- src/main/java/qa/pageobject/shoppingcart/Row.java
+++ src/main/java/qa/pageobject/shoppingcart/Row.java
@@ -3,10 +3,8 @@ package qa.pageobject.shoppingcart;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import qa.base.BasePage;
 import qa.pageobject.productpage.QuantityField;
-import qa.tools.toby.ToBy;
 
 
 public class Row extends BasePage {
@@ -43,7 +41,6 @@ public class Row extends BasePage {
 
     public String getSubtotal() throws IllegalAccessException {
 
-        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(ToBy.get(parent.findElement(By.className("product-subtotal")))));
         return parent.findElement(By.className("product-subtotal")).getText();
     }
 
