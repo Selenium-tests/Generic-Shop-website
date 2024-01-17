63dbe6bb20c4377b385621538d045c85d871fa50
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-01-17 08:53:42 +0100

    Added methods

--- src/test/java/tests/shoppingcart/AddingProductToTheCartFromProductPageTest.java
+++ src/test/java/tests/shoppingcart/AddingProductToTheCartFromProductPageTest.java
@@ -4,12 +4,11 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import qa.base.BaseTest;
-import qa.pageobject.header.Header;
+import qa.enums.URLs;
 import qa.pageobject.productpage.ProductPage;
 import qa.pageobject.shoppingcart.Row;
 import qa.pageobject.shoppingcart.ShoppingCart;
-import qa.provider.MyDataProvider;
-import qa.extentreports.ExtentReportsManager;
+import qa.dataproviders.DataProviders;
 import qa.data.Link;
 
 
@@ -17,93 +16,86 @@ public class AddingProductToTheCartFromProductPageTest extends BaseTest {
 
     private ProductPage productPage;
     private ShoppingCart shoppingCart;
-    private Header header;
+    private String productName;
+    private String productPrice;
 
     @BeforeMethod
     public void create() {
 
         productPage = new ProductPage(getDriver());
         shoppingCart = new ShoppingCart(getDriver());
-        header = new Header(getDriver());
     }
 
-    private void check() throws IllegalAccessException {
+    private void addToCart() throws IllegalAccessException {
 
-        String name = productPage.getProductTitle();
-        String price = productPage.getPrice();
+        productName = productPage.getProductTitle();
+        productPrice = productPage.getPrice();
 
         productPage.getQuantityField().setQuantity("1");
         productPage.clickAddToCart();
-        header.clickCartButton();
+    }
+
+    private void checkShoppingCartContents() {
 
         Assert.assertFalse(shoppingCart.getContentsLocator().isEmpty(), "The shopping cart is empty");
+    }
+
+    private void checkRow() {
 
         Row row = shoppingCart.getTable().getRow(0);
 
-        Assert.assertEquals(row.getName(), name, "Incorrect product name in the shopping cart");
-        Assert.assertEquals(row.getPrice(), price, "Incorrect price in the shopping cart");
+        Assert.assertEquals(row.getName(), productName, "Incorrect product name in the shopping cart");
+        Assert.assertEquals(row.getPrice(), productPrice, "Incorrect price in the shopping cart");
     }
 
-    @Test(dataProvider = "allBlackTops", dataProviderClass = MyDataProvider.class)
+    private void actions(String productPageUrl) throws IllegalAccessException {
+
+        goToSpecificPage(productPageUrl);
+        addToCart();
+        goToSpecificPage(URLs.SHOPPING_CART.getName());
+        checkShoppingCartContents();
+        checkRow();
+    }
+
+    @Test(dataProvider = "allBlackTops", dataProviderClass = DataProviders.class)
     public void allBlackTops(Link link) throws IllegalAccessException {
 
-        ExtentReportsManager.setName("Adding the \"" + link.getLinkText() + "\" product from the \"ALL BLACK TOPS\" category");
-
-        goToSpecificPage(link.getPageURL());
-        check();
+        actions(link.getPageURL());
     }
 
-    @Test(dataProvider = "highHeelShoesProducts", dataProviderClass = MyDataProvider.class)
+    @Test(dataProvider = "highHeelShoesProducts", dataProviderClass = DataProviders.class)
     public void highHeelShoes(Link link) throws IllegalAccessException {
 
-        ExtentReportsManager.setName("Adding the \"" + link.getLinkText() + "\" product from the \"HIGH HEEL SHOES\" category");
-
-        goToSpecificPage(link.getPageURL());
-        check();
+        actions(link.getPageURL());
     }
 
-    @Test(dataProvider = "mostWantedProducts", dataProviderClass = MyDataProvider.class)
+    @Test(dataProvider = "mostWantedProducts", dataProviderClass = DataProviders.class)
     public void mostWanted(Link link) throws IllegalAccessException {
 
-        ExtentReportsManager.setName("Adding the \"" + link.getLinkText() + "\" product from the \"MOST WANTED\" category");
-
-        goToSpecificPage(link.getPageURL());
-        check();
+        actions(link.getPageURL());
     }
 
-    @Test(dataProvider = "scarfsProducts", dataProviderClass = MyDataProvider.class)
+    @Test(dataProvider = "scarfsProducts", dataProviderClass = DataProviders.class)
     public void scarfs(Link link) throws IllegalAccessException {
 
-        ExtentReportsManager.setName("Adding the \"" + link.getLinkText() + "\" product from the \"SCARFS\" category");
-
-        goToSpecificPage(link.getPageURL());
-        check();
+        actions(link.getPageURL());
     }
 
-    @Test(dataProvider = "onSaleProducts", dataProviderClass = MyDataProvider.class)
+    @Test(dataProvider = "onSaleProducts", dataProviderClass = DataProviders.class)
     public void onSale(Link link) throws IllegalAccessException {
 
-        ExtentReportsManager.setName("Adding the \"" + link.getLinkText() + "\" product from the \"ON SALE\" category");
-
-        goToSpecificPage(link.getPageURL());
-        check();
+        actions(link.getPageURL());
     }
 
-    @Test(dataProvider = "featuredProducts", dataProviderClass = MyDataProvider.class)
+    @Test(dataProvider = "featuredProducts", dataProviderClass = DataProviders.class)
     public void featured(Link link) throws IllegalAccessException {
 
-        ExtentReportsManager.setName("Adding the \"" + link.getLinkText() + "\" product from the \"FEATURED\" category");
-
-        goToSpecificPage(link.getPageURL());
-        check();
+        actions(link.getPageURL());
     }
 
-    @Test(dataProvider = "trendsProducts", dataProviderClass = MyDataProvider.class)
+    @Test(dataProvider = "trendsProducts", dataProviderClass = DataProviders.class)
     public void trends(Link link) throws IllegalAccessException {
 
-        ExtentReportsManager.setName("Adding the \"" + link.getLinkText() + "\" product from the \"TRENDS\" category");
-
-        goToSpecificPage(link.getPageURL());
-        check();
+        actions(link.getPageURL());
     }
 }
