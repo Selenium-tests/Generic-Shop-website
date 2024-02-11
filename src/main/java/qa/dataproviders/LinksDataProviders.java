0ecc387617ecf62468a3941cbbf67c7668873292
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-11 12:18:19 +0100

    Updated

--- src/main/java/qa/dataproviders/LinksDataProviders.java
+++ src/main/java/qa/dataproviders/LinksDataProviders.java
@@ -9,55 +9,55 @@ public class LinksDataProviders {
     @DataProvider(name = DataProviderNames.MAIN_MENU)
     public Object[] mainMenu() {
 
-        return ModelsBuilder.getLink(DataProviderNames.MAIN_MENU);
+        return ModelsBuilder.getLinkData(DataProviderNames.MAIN_MENU);
     }
 
     @DataProvider(name = DataProviderNames.MAIN_MENU_DROPDOWN_LIST)
     public Object[] mainMenuDropdownList() {
 
-        return ModelsBuilder.getLink(DataProviderNames.MAIN_MENU_DROPDOWN_LIST);
+        return ModelsBuilder.getLinkData(DataProviderNames.MAIN_MENU_DROPDOWN_LIST);
     }
 
     @DataProvider(name = DataProviderNames.ALL_BLACK_TOPS)
     public Object[] allBlackTops() {
 
-        return ModelsBuilder.getLink(DataProviderNames.ALL_BLACK_TOPS);
+        return ModelsBuilder.getLinkData(DataProviderNames.ALL_BLACK_TOPS);
     }
 
     @DataProvider(name = DataProviderNames.HIGH_HEEL_SHOES)
     public Object[] highHeelShoes() {
 
-        return ModelsBuilder.getLink(DataProviderNames.HIGH_HEEL_SHOES);
+        return ModelsBuilder.getLinkData(DataProviderNames.HIGH_HEEL_SHOES);
     }
 
     @DataProvider(name = DataProviderNames.MOST_WANTED)
     public Object[] mostWanted() {
 
-        return ModelsBuilder.getLink(DataProviderNames.MOST_WANTED);
+        return ModelsBuilder.getLinkData(DataProviderNames.MOST_WANTED);
     }
 
     @DataProvider(name = DataProviderNames.SCARFS)
     public Object[] scarfsProducts() {
 
-        return ModelsBuilder.getLink(DataProviderNames.SCARFS);
+        return ModelsBuilder.getLinkData(DataProviderNames.SCARFS);
     }
 
     @DataProvider(name = DataProviderNames.ON_SALE)
     public Object[] onSaleProducts() {
 
-        return ModelsBuilder.getLink(DataProviderNames.ON_SALE);
+        return ModelsBuilder.getLinkData(DataProviderNames.ON_SALE);
     }
 
     @DataProvider(name = DataProviderNames.FEATURED)
     public Object[] featured() {
 
-        return ModelsBuilder.getLink(DataProviderNames.FEATURED);
+        return ModelsBuilder.getLinkData(DataProviderNames.FEATURED);
     }
 
     @DataProvider(name = "trendsProducts")
     public Object[] trends() {
 
-        return ModelsBuilder.getLink("trendsProducts");
+        return ModelsBuilder.getLinkData("trendsProducts");
     }
 
 }
