71521edd134720c570e38b6a41e05ca87a6b119c
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-11 12:21:41 +0100

    Added the method

--- src/main/java/qa/testdataloader/TestdataLoader.java
+++ src/main/java/qa/testdataloader/TestdataLoader.java
@@ -1,11 +1,8 @@
 package qa.testdataloader;
 
-import com.google.gson.JsonParser;
 import lombok.Getter;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
@@ -16,15 +13,20 @@ public class TestdataLoader {
     @Getter
     private static String source;
 
-    public static void load(String filename) {
+    public static String fetch(String filename) {
 
         try {
             Reader reader = new FileReader(path + filename + ".json");
             JSONParser jsonParser = new JSONParser();
             Object object = jsonParser.parse(reader);
-            source = object.toString();
+            return object.toString();
         } catch (IOException | ParseException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public static void load(String filename) {
+
+        source = fetch(filename);
     }
 }
