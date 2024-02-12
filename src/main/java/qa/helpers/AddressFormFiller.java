97918f545930e7827555c7d97512525d198857aa
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-12 08:18:44 +0100

    Updated

--- src/main/java/qa/helpers/AddressFormFiller.java
+++ src/main/java/qa/helpers/AddressFormFiller.java
@@ -2,29 +2,20 @@ package qa.helpers;
 
 import org.openqa.selenium.WebDriver;
 import qa.pageobject.addressform.AddressForm;
-import qa.data.AddressFormData;
+import qa.data.AddressData;
 
 
 public class AddressFormFiller {
 
-    public static AddressForm get(AddressFormData data, WebDriver driver, boolean pressEnterAgain) throws IllegalAccessException {
+    public static AddressForm get(AddressData data, WebDriver driver) throws IllegalAccessException {
 
         AddressForm addressForm = new AddressForm(driver);
-
-        addressForm.getCountryDropdownList().clickCountryButton();
         addressForm.getCountryDropdownList().setCountry(data.getCountry());
-        addressForm.getCountryDropdownList().pressEnter();
-
-        if (pressEnterAgain) {
-
-            addressForm.getCountryDropdownList().pressEnter();
-        }
-
         addressForm.setFirstName(data.getFirstName());
         addressForm.setLastName(data.getLastName());
         addressForm.setCompany(data.getCompanyName());
-        addressForm.setAddress_1(data.getAddress1());
-        addressForm.setAddress_2(data.getAddress2());
+        addressForm.setAddress_1(data.getAddress());
+        addressForm.setAddress_2(data.getAddressLine2());
         addressForm.setCity(data.getCity());
         addressForm.setPostcode(data.getPostcode());
         addressForm.setPhone(data.getPhone());
