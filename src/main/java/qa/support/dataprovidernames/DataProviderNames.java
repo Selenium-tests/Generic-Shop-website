01075c7d03d7f39b4cf7aef988261bce3ebc1881
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-12 18:20:26 +0100

    Added fields

--- src/main/java/qa/support/dataprovidernames/DataProviderNames.java
+++ src/main/java/qa/support/dataprovidernames/DataProviderNames.java
@@ -29,4 +29,30 @@ public class DataProviderNames {
     public static final String ACCOUNT_NAVIGATION = "accountNavigation";
     public static final String BILLING_ADDRESS_FORM = "billingAddressForm";
     public static final String SHIPPING_ADDRESS_FORM = "shippingAddressForm";
+    public static final String INCORRECT_FIRST_NAME = "incorrectFirstName";
+    public static final String INCORRECT_LAST_NAME = "incorrectLastName";
+    public static final String INCORRECT_POSTCODE = "incorrectPostcode";
+    public static final String INCORRECT_PHONE = "incorrectPhone";
+    public static final String INCORRECT_CITY = "incorrectCity";
+    public static final String INCORRECT_ADDRESS = "incorrectAddress";
+    public static final String BLANK_FIRST_NAME_FIELD = "blankFirstNameField";
+    public static final String BLANK_LAST_NAME_FIELD = "blankLastNameField";
+    public static final String BLANK_COMPANY_FIELD = "blankCompanyField";
+    public static final String BLANK_ADDRESS_FIELD = "blankAddressField";
+    public static final String BLANK_ADDRESS_FIELD_LINE_2 = "blankAddressFieldLine2";
+    public static final String BLANK_CITY_FIELD = "blankCityField";
+    public static final String BLANK_POSTCODE_FIELD = "blankPostcodeField";
+    public static final String BLANK_PHONE_FIELD = "blankPhoneField";
+    public static final String STATE_COUNTY_FIELD = "stateCountyField";
+    public static final String STATE_COUNTY_DROPDOWN_LIST = "stateCountyDropdownList";
+    public static final String COUNTY_DROPDOWN_LIST = "countyDropdownList";
+    public static final String STATE_DROPDOWN_LIST = "stateDropdownList";
+    public static final String DISTRICT_DROPDOWN_LIST = "districtDropdownList";
+    public static final String PROVINCE_DROPDOWN_LIST = "provinceDropdownList";
+    public static final String REGION_FIELD = "regionField";
+    public static final String REGION_DROPDOWN_LIST = "regionDropdownList";
+    public static final String PREFECTURE_DROPDOWN_LIST = "prefectureDropdownList";
+    public static final String MUNICIPALITY_FIELD = "municipalityField";
+    public static final String STATE_ZONE_DROPDOWN_LIST = "stateZoneDropdownList";
+
 }
