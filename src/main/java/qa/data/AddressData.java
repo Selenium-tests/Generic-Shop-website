79b8f2a4d973ac178b58079d7870c8bfcde1ed54
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-12 08:09:41 +0100

    Renamed

--- src/main/java/qa/data/AddressData.java
+++ src/main/java/qa/data/AddressData.java
@@ -2,30 +2,31 @@ package qa.data;
 
 
 import lombok.Getter;
+import qa.pageobject.addressform.AddressForm;
 
 @Getter
-public class AddressFormData {
+public class AddressData {
 
     private final String country;
     private final String firstName;
     private final String lastName;
     private final String companyName;
-    private final String address1;
-    private final String address2;
+    private final String address;
+    private final String addressLine2;
     private final String city;
     private final String postcode;
     private final String phone;
     private final String email;
     private final String errorMessage;
 
-    public AddressFormData(String country, String firstName, String lastName, String companyName, String address1, String address2, String city, String postcode, String phone, String email, String errorMessage) {
+    public AddressData(String country, String firstName, String lastName, String companyName, String address, String addressLine2, String city, String postcode, String phone, String email, String errorMessage) {
 
         this.country = country;
         this.firstName = firstName;
         this.lastName = lastName;
         this.companyName = companyName;
-        this.address1 = address1;
-        this.address2 = address2;
+        this.address = address;
+        this.addressLine2 = addressLine2;
         this.city = city;
         this.postcode = postcode;
         this.phone = phone;
