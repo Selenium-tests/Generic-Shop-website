f9fe1e2ddab204bd2e3a47457c08e11b053e7b58
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-13 18:28:25 +0100

    Renamed

---
+++ src/main/java/qa/jsonreader/ModelsBuilder.java
@@ -0,0 +1,93 @@
+package qa.jsonreader;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import qa.data.*;
+import qa.testdataloader.TestdataLoader;
+
+import java.util.stream.IntStream;
+
+
+public class ModelsBuilder {
+    public static Credentials[] getCredentials(String key) {
+
+        String source = TestdataLoader.getSource();
+        JSONObject jsonObject = new JSONObject(source);
+        JSONArray jsonArray = jsonObject.getJSONArray(key);
+
+        return IntStream.range(0, jsonArray.length())
+                .mapToObj(i -> new Credentials(
+                        jsonArray.getJSONObject(i).getString("emailOrUsername"),
+                        jsonArray.getJSONObject(i).getString("password"),
+                        jsonArray.getJSONObject(i).getString("message")
+                ))
+                .toArray(Credentials[]::new);
+    }
+
+    public static String[] getStrings(String key) {
+
+        String source = TestdataLoader.getSource();
+        JSONObject jsonObject = new JSONObject(source);
+        JSONArray jsonArray = jsonObject.getJSONArray(key);
+
+        return IntStream.range(0, jsonArray.length())
+                .mapToObj(jsonArray::getString)
+                .toArray(String[]::new);
+    }
+
+    public static LinkData[] getLinkData(String key) {
+
+        String source = TestdataLoader.getSource();
+        JSONObject jsonObject = new JSONObject(source);
+        JSONArray jsonArray = jsonObject.getJSONArray(key);
+
+        return IntStream.range(0, jsonArray.length())
+                .mapToObj(i -> new LinkData(
+                        jsonArray.getJSONObject(i).getString("link"),
+                        jsonArray.getJSONObject(i).getString("url")
+                ))
+                .toArray(LinkData[]::new);
+    }
+
+    public static NewsletterData[] getNewsletterData(String key) {
+
+        String source = TestdataLoader.getSource();
+        JSONObject jsonObject = new JSONObject(source);
+        JSONArray jsonArray = jsonObject.getJSONArray(key);
+
+        return IntStream.range(0, jsonArray.length())
+                .mapToObj(i -> new NewsletterData(
+                        jsonArray.getJSONObject(i).getString("username"),
+                        jsonArray.getJSONObject(i).getString("email"),
+                        jsonArray.getJSONObject(i).getString("message")
+                ))
+                .toArray(NewsletterData[]::new);
+    }
+
+    public static AddressData[] getAddressFormData(String key) {
+
+        return getAddressFormData(key, TestdataLoader.getSource());
+    }
+
+    public static AddressData[] getAddressFormData(String key, String source) {
+
+        JSONObject jsonObject = new JSONObject(source);
+        JSONArray jsonArray = jsonObject.getJSONArray(key);
+
+        return IntStream.range(0, jsonArray.length())
+                .mapToObj(i -> new AddressData(
+                        jsonArray.getJSONObject(i).getString("country"),
+                        jsonArray.getJSONObject(i).getString("firstName"),
+                        jsonArray.getJSONObject(i).getString("lastName"),
+                        jsonArray.getJSONObject(i).getString("company"),
+                        jsonArray.getJSONObject(i).getString("address"),
+                        jsonArray.getJSONObject(i).getString("addressLine2"),
+                        jsonArray.getJSONObject(i).getString("city"),
+                        jsonArray.getJSONObject(i).getString("postcode"),
+                        jsonArray.getJSONObject(i).getString("phone"),
+                        jsonArray.getJSONObject(i).getString("email"),
+                        jsonArray.getJSONObject(i).getString("message")
+                ))
+                .toArray(AddressData[]::new);
+    }
+}
