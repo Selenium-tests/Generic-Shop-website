a2d2e248b7304d961082189377eb05ffbac222cf
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-14 08:35:09 +0100

    Moved

---
+++ src/main/java/qa/quantityfield/QuantityField.java
@@ -0,0 +1,41 @@
+package qa.quantityfield;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import qa.base.BasePage;
+
+public class QuantityField extends BasePage {
+
+    private final String inputFieldSelector;
+    private final WebElement parent;
+
+    public QuantityField(WebDriver driver, WebElement parent) {
+
+        super(driver);
+
+        this.inputFieldSelector = "input[type='number']";
+        this.parent = parent;
+    }
+
+    public void setQuantity(String quantity) throws IllegalAccessException {
+
+        WebElement element = getWebDriverWait().until(ExpectedConditions.elementToBeClickable(
+                parent.findElement(By.cssSelector(inputFieldSelector))));
+        element.clear();
+        element.sendKeys(quantity);
+    }
+
+    public String getMin() {
+
+        return getWebDriverWait().until(ExpectedConditions.elementToBeClickable(
+                parent.findElement(By.cssSelector(inputFieldSelector)))).getAttribute("min");
+    }
+
+    public String getValidationMessage() {
+
+        return getWebDriverWait().until(ExpectedConditions.elementToBeClickable(
+                parent.findElement(By.cssSelector(inputFieldSelector)))).getAttribute("validationMessage");
+    }
+}
