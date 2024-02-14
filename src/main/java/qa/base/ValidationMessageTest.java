e00d79ca9d7778556068f049ab5879557f7ce43f
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-14 06:21:30 +0100

    Removed the method

--- src/main/java/qa/base/ValidationMessageTest.java
+++ src/main/java/qa/base/ValidationMessageTest.java
@@ -10,10 +10,4 @@ public class ValidationMessageTest extends BaseTest {
         Assert.assertNotEquals(quantityField.getValidationMessage(), "",
                 "No validation message");
     }
-
-    protected void checkValidationMessageContent(QuantityField quantityField, String expectedValidationMessage) {
-
-        Assert.assertTrue(quantityField.getValidationMessage().contains(expectedValidationMessage),
-                "Incorrect validation message content");
-    }
 }
