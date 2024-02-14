d644e36e72ee1b9c2adaa9c8b5c38d5e20b25dbf
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-14 18:32:38 +0100

    Created

---
+++ src/main/java/qa/data/ProductData.java
@@ -0,0 +1,18 @@
+package qa.data;
+
+import lombok.Getter;
+
+@Getter
+public class ProductData {
+
+    private final String name;
+    private final String price;
+    private final String quantity;
+
+    public ProductData(String name, String price, String quantity) {
+
+        this.name = name;
+        this.price = price;
+        this.quantity = quantity;
+    }
+}
