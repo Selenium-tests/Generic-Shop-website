19fa1de44f36a3a398bcec968e41aabd75053920
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-14 18:41:43 +0100

    Added methods

--- src/main/java/qa/pageobject/shoppingcart/Table.java
+++ src/main/java/qa/pageobject/shoppingcart/Table.java
@@ -5,21 +5,56 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import qa.base.BasePage;
+import qa.quantityfield.QuantityField;
+
+import java.util.List;
 
 public class Table extends BasePage {
 
+    private final String rowSelector;
+    private List<WebElement> rows;
     private final Row row;
 
     public Table(WebDriver driver) {
 
         super(driver);
 
+        rowSelector = ".woocommerce-cart-form__cart-item.cart_item";
         row = new Row(driver);
     }
 
-    @FindBy(xpath = ".//table[@class='shop_table shop_table_responsive cart woocommerce-cart-form__contents']")
+    @FindBy(css = "table.shop_table.shop_table_responsive.cart.woocommerce-cart-form__contents")
     WebElement contents;
 
+    public void findRows() {
+
+        rows = contents.findElements(By.cssSelector(rowSelector));
+    }
+
+    public int getRowsCount() {
+
+        return rows.size();
+    }
+
+    public String getProductName(int row) {
+
+        return rows.get(row).findElement(By.className("product-name")).getText();
+    }
+
+    public String getPrice(int row) {
+
+        return rows.get(row).findElement(By.className("product-price")).getText();
+    }
+
+    public String getTotal(int row) {
+
+        return rows.get(row).findElement(By.className("product-subtotal")).getText();
+    }
+
+    public QuantityField getQuantityField(int row) {
+
+        return new QuantityField(getDriver(), rows.get(row));
+    }
 
     public Row getRow(int index) {
 
