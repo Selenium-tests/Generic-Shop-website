6d7f9279383bca754b4cb00fb3941d99b332e176
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-16 20:10:35 +0100

    Added the inheritance

--- src/test/java/tests/shoppingcart/AddingFromProductPageTest.java
+++ src/test/java/tests/shoppingcart/AddingFromProductPageTest.java
@@ -1,22 +1,19 @@
 package tests.shoppingcart;
 
-import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import org.testng.asserts.SoftAssert;
-import qa.base.BaseTest;
 import qa.data.ProductData;
 import qa.data.ProductToCart;
 import qa.dataproviders.ProductsToCartDataProvider;
-import qa.enums.URLs;
 import qa.pageobject.productpage.ProductPage;
 import qa.pageobject.shoppingcart.ShoppingCart;
+import tests.base.AddingProductToShoppingCartBaseTest;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
-public class AddingFromProductPageTest extends BaseTest {
+public class AddingFromProductPageTest extends AddingProductToShoppingCartBaseTest {
 
     private ProductPage productPage;
     private ShoppingCart shoppingCart;
@@ -35,39 +32,9 @@ public class AddingFromProductPageTest extends BaseTest {
 
     private void addToCart(ProductToCart data) throws IllegalAccessException {
 
-        productData.add(new ProductData(
-                productPage.getProductName(),
-                productPage.getPrice(),
-                data.getQuantity()
-        ));
-
+        productData.add(new ProductData(productPage.getProductName(), productPage.getPrice(), data.getQuantity()));
         productPage.getQuantityField().setQuantity(data.getQuantity());
         productPage.clickAddToCart();
-    }
-
-    private void checkShoppingCartRowsCount() {
-
-        goToSpecificPage(URLs.SHOPPING_CART.getName());
-        shoppingCart.getTable().findRows();
-
-        Assert.assertEquals(shoppingCart.getTable().getRowsCount(), productsToCart.length);
-    }
-
-    private void checkShoppingCartTableCells() {
-
-        SoftAssert softAssert = new SoftAssert();
-
-        for (int i = 0; i < shoppingCart.getTable().getRowsCount(); i++) {
-
-            softAssert.assertEquals(shoppingCart.getTable().getRow(i).getName(), productData.get(i).getName(),
-                    "Incorrect product name");
-            softAssert.assertEquals(shoppingCart.getTable().getRow(i).getPrice(), productData.get(i).getPrice(),
-                    "Incorrect price");
-            softAssert.assertEquals(shoppingCart.getTable().getQuantityField(i).getValue(), productData.get(i).getQuantity(),
-                    "Incorrect quantity");
-        }
-
-        softAssert.assertAll();
     }
 
     private void actions(ProductToCart data) throws IllegalAccessException {
@@ -84,7 +51,7 @@ public class AddingFromProductPageTest extends BaseTest {
             actions(pc);
         }
 
-        checkShoppingCartRowsCount();
-        checkShoppingCartTableCells();
+        checkShoppingCartRowsCount(shoppingCart, productsToCart.length);
+        checkShoppingCartTableCells(shoppingCart, productData);
     }
 }
