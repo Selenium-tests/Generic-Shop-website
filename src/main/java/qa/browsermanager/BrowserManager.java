fb6a94c12049082ed0ee24e44f0d68fd48716689
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-16 20:12:51 +0100

    Added the method

--- src/main/java/qa/browsermanager/BrowserManager.java
+++ src/main/java/qa/browsermanager/BrowserManager.java
@@ -5,10 +5,18 @@ import org.openqa.selenium.remote.RemoteWebDriver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.time.Duration;
+
 
 public class BrowserManager {
 
     private static final Logger logger = LoggerFactory.getLogger(BrowserManager.class);
+
+    public static void start(WebDriver driver) {
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.manage().window().maximize();
+    }
 
     public static void goToPage(WebDriver driver, String url) {
 
