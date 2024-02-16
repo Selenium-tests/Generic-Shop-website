af7af4e25da24a6fd6ef9a4faf509deb51af6ef9
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-02-16 20:18:35 +0100

    Updated the import

--- src/main/java/qa/pageobject/mainmenu/DropdownList.java
+++ src/main/java/qa/pageobject/mainmenu/DropdownList.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import qa.base.BasePage;
-import qa.tools.toby.ToBy;
+import qa.support.toby.ToBy;
 
 public class DropdownList extends BasePage {
 
