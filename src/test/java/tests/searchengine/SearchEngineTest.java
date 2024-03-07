5c0c80d6daa05d97834ee24b80b53c5e80eb6336
Paweł Aksman <44415622+aksi1984@users.noreply.github.com>
2024-03-07 18:04:11 +0100

    Added annotations

--- src/test/java/tests/searchengine/SearchEngineTest.java
+++ src/test/java/tests/searchengine/SearchEngineTest.java
@@ -1,5 +1,10 @@
-package tests.homepage;
+package tests.searchengine;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qase.api.annotation.QaseId;
+import io.qase.api.annotation.QaseTitle;
 import tests.base.BaseTest;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
@@ -12,7 +17,8 @@ import qa.support.dataprovidernames.DataProviderNames;
 
 import java.util.function.Consumer;
 
-
+@Epic("E2E")
+@Feature("Search engine")
 public class SearchEngineTest extends BaseTest {
 
     private SearchEngine searchEngine;
@@ -34,7 +40,21 @@ public class SearchEngineTest extends BaseTest {
         consumer.accept(resultsPage);
     }
 
+    @Test
+    @QaseId(5)
+    @QaseTitle("Text input verification")
+    @Description("Text input verification")
+    public void textInputVerification() throws IllegalAccessException {
+
+        String text = "This is the text";
+        searchEngine.setPhrase(text);
+        Assert.assertEquals(searchEngine.getPhrase(), text, "Incorrect output");
+    }
+
     @Test(priority = 1, dataProvider = DataProviderNames.CORRECT, dataProviderClass = PhrasesDataProviders.class)
+    @QaseId(6)
+    @QaseTitle("Searching with a correct phrase")
+    @Description("Searching with a correct phrase")
     public void correct(String phrase) throws IllegalAccessException {
 
         check(phrase, (ResultsPage rp)-> Assert.assertFalse(rp.hasNoResults(),
@@ -42,6 +62,9 @@ public class SearchEngineTest extends BaseTest {
     }
 
     @Test(priority = 2, dataProvider = DataProviderNames.PARTIAL, dataProviderClass = PhrasesDataProviders.class)
+    @QaseId(7)
+    @QaseTitle("Searching with an incorrect phrase")
+    @Description("Searching with an incorrect phrase")
     public void partial(String phrase) throws IllegalAccessException {
 
         check(phrase, (ResultsPage rp)-> Assert.assertFalse(rp.hasNoResults(),
