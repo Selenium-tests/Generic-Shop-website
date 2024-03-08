d94df6b1972512a2d368ba4040945e679d2e4f78
pawelaksman <pablo_xmen@op.pl>
2024-03-08 23:57:29 +0100

    Added data providers

--- src/main/java/qa/dataproviders/NewsletterDataProviders.java
+++ src/main/java/qa/dataproviders/NewsletterDataProviders.java
@@ -2,9 +2,46 @@ package qa.dataproviders;
 
 import org.testng.annotations.DataProvider;
 import qa.jsonreader.ModelsBuilder;
-import qa.support.dataprovidernames.DataProviderNames;
+import qa.support.constans.CommonConstans;
+import qa.support.constans.DataProviderNames;
 
 public class NewsletterDataProviders {
+
+    @DataProvider(name = DataProviderNames.NAME_FIELD_BELOW_MAX)
+    public Object[] nameFieldBelowMax() {
+
+        return ModelsBuilder.getNewsletterFieldText("nameField", CommonConstans.BELOW_MAX);
+    }
+
+    @DataProvider(name = DataProviderNames.NAME_FIELD_MAX)
+    public Object[] nameFieldMax() {
+
+        return ModelsBuilder.getNewsletterFieldText("nameField", CommonConstans.MAX);
+    }
+
+    @DataProvider(name = DataProviderNames.NAME_FIELD_ABOVE_MAX)
+    public Object[] nameFieldAboveMax() {
+
+        return ModelsBuilder.getNewsletterFieldText("nameField", CommonConstans.ABOVE_MAX);
+    }
+
+    @DataProvider(name = DataProviderNames.EMAIL_FIELD_BELOW_MAX)
+    public Object[] emailFieldBelowMax() {
+
+        return ModelsBuilder.getNewsletterFieldText("emailField", CommonConstans.BELOW_MAX);
+    }
+
+    @DataProvider(name = DataProviderNames.EMAIL_FIELD_MAX)
+    public Object[] emailFieldMax() {
+
+        return ModelsBuilder.getNewsletterFieldText("emailField", CommonConstans.MAX);
+    }
+
+    @DataProvider(name = DataProviderNames.EMAIL_FIELD_ABOVE_MAX)
+    public Object[] emailFieldAboveMax() {
+
+        return ModelsBuilder.getNewsletterFieldText("emailField", CommonConstans.ABOVE_MAX);
+    }
 
     @DataProvider(name = DataProviderNames.CORRECT)
     public Object[] correct() {
