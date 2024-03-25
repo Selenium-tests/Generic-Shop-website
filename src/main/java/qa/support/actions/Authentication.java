bb56507c5fe52e20a3b793d55f2a25931a792076
pawelaksman <pablo_xmen@op.pl>
2024-03-25 23:50:48 +0100

    Updated

--- src/main/java/qa/support/actions/Authentication.java
+++ src/main/java/qa/support/actions/Authentication.java
@@ -1,15 +1,17 @@
 package qa.support.actions;
 
 import org.openqa.selenium.WebDriver;
+import qa.support.constans.DataProviderNames;
 import qa.support.modelsbuilder.ModelsBuilder;
 import qa.models.Credentials;
 import qa.pageobject.loginform.LoginForm;
+import qa.support.testdataloader.TestdataLoader;
 
 public class Authentication {
 
     public static void loginWithCredentials(WebDriver driver) throws IllegalAccessException {
 
-        Credentials[] credentials = ModelsBuilder.getCorrectCredentials();
+        Credentials[] credentials = ModelsBuilder.getCredentials(TestdataLoader.loadQuickly("GSS_Credentials"), DataProviderNames.CORRECT);
         LoginForm loginForm = new LoginForm(driver);
         loginForm.setUsername(credentials[0].getEmailOrUsername());
         loginForm.setPassword(credentials[0].getPassword());
