01e7f86034f3af0668e7a2e6e749ace7bfc09416
pawelaksman <pablo_xmen@op.pl>
2024-03-29 16:06:24 +0100

    Created

---
+++ src/test/java/tests/account/addressform/TextInputValidationTest.java
@@ -0,0 +1,125 @@
+package tests.account.addressform;
+
+import io.qameta.allure.Description;
+import io.qase.api.annotation.QaseId;
+import io.qase.api.annotation.QaseTitle;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import qa.enums.URLs;
+import qa.pageobject.addressform.AddressForm;
+import qa.support.actions.Authentication;
+import tests.base.BaseTest;
+
+public class TextInputValidationTest extends BaseTest {
+
+    private AddressForm addressForm;
+    private final String FIRST_NAME = "John";
+    private final String LAST_NAME = "Doe";
+    private final String COMPANY = "company";
+    private final String ADDRESS_1 = "address 1";
+    private final String ADDRESS_2 = "address 2";
+    private final String POSTCODE = "22333";
+    private final String CITY = "New York";
+    private final String PHONE = "111222333";
+    private final String EMAIL = "jdoe@domain.com";
+
+    @BeforeMethod
+    public void create() throws IllegalAccessException {
+
+        goToPage(URLs.LOGIN_PAGE.getName());
+        Authentication.loginWithCredentials(getDriver());
+        goToPage(URLs.BILLING_ADDRESS_FORM.getName());
+        addressForm = new AddressForm(getDriver());
+    }
+
+    @Test(priority = 1)
+    @QaseId(91)
+    @QaseTitle("Verification of the \"First name\" field text input\"")
+    @Description("Verification of the \"First name\" field text input\"")
+    public void firstNameField() {
+
+        addressForm.setFirstName(FIRST_NAME);
+        Assert.assertEquals(addressForm.getFirstName(), FIRST_NAME, "Incorrect the \"First name\" field output");
+    }
+
+    @Test(priority = 2)
+    @QaseId(92)
+    @QaseTitle("Verification of the \"Last name\" field text input\"")
+    @Description("Verification of the \"Last name\" field text input\"")
+    public void lastNameField() {
+
+        addressForm.setLastName(LAST_NAME);
+        Assert.assertEquals(addressForm.getLastName(), LAST_NAME, "Incorrect the \"Last name\" field output");
+    }
+
+    @Test(priority = 3)
+    @QaseId(93)
+    @QaseTitle("Verification of the \"Company name\" field text input\"")
+    @Description("Verification of the \"Company name\" field text input\"")
+    public void companyField() {
+
+        addressForm.setCompany(COMPANY);
+        Assert.assertEquals(addressForm.getCompany(), COMPANY, "Incorrect the \"Company name\" field output");
+    }
+
+    @Test(priority = 4)
+    @QaseId(94)
+    @QaseTitle("Verification of the \"Street address\" field text input\"")
+    @Description("Verification of the \"Street address\" field text input\"")
+    public void addressField() {
+
+        addressForm.setAddress_1(ADDRESS_1);
+        Assert.assertEquals(addressForm.getAddress(), ADDRESS_1, "Incorrect the \"Street address\" field output");
+    }
+
+    @Test(priority = 5)
+    @QaseId(95)
+    @QaseTitle("Verification of the \"Street address\" optional field text input\"")
+    @Description("Verification of the \"Street address\" optional field text input\"")
+    public void addressField_2() {
+
+        addressForm.setAddress_2(ADDRESS_2);
+        Assert.assertEquals(addressForm.getAddressLine2(), ADDRESS_2, "Incorrect the \"Street address\" optional field output");
+    }
+
+    @Test(priority = 6)
+    @QaseId(96)
+    @QaseTitle("Verification of the \"Postcode / ZIP\" field text input\"")
+    @Description("Verification of the \"Postcode / ZIP\" field text input\"")
+    public void postcodeField() {
+
+        addressForm.setPostcode(POSTCODE);
+        Assert.assertEquals(addressForm.getPostcode(), POSTCODE, "Incorrect the \"Postcode / ZIP\" field output");
+    }
+
+    @Test(priority = 7)
+    @QaseId(97)
+    @QaseTitle("Verification of the \"Town / City\" field text input\"")
+    @Description("Verification of the \"Town / City\" field text input\"")
+    public void cityField() {
+
+        addressForm.setCity(CITY);
+        Assert.assertEquals(addressForm.getCity(), CITY, "Incorrect the \"Town / City\" field output");
+    }
+
+    @Test(priority = 8)
+    @QaseId(98)
+    @QaseTitle("Verification of the \"Phone\" field text input\"")
+    @Description("Verification of the \"Phone\" field text input\"")
+    public void phoneField() {
+
+        addressForm.setPhone(PHONE);
+        Assert.assertEquals(addressForm.getPhone(), PHONE, "Incorrect the \"Phone\" field output");
+    }
+
+    @Test(priority = 9)
+    @QaseId(99)
+    @QaseTitle("Verification of the \"Email address\" field text input\"")
+    @Description("Verification of the \"Email address\" field text input\"")
+    public void emailField() {
+
+        addressForm.setEmail(EMAIL);
+        Assert.assertEquals(addressForm.getEmail(), EMAIL, "Incorrect the \"Email address\" field output");
+    }
+}
