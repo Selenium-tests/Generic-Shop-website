14accec02d1b4e12f09e662b05cdca2478b6489f
pawelaksman <pablo_xmen@op.pl>
2024-04-15 23:59:49 +0200

    Added annotations

--- src/test/java/tests/login/InputTextVerificationTest.java
+++ src/test/java/tests/login/InputTextVerificationTest.java
@@ -1,48 +1,60 @@
 package tests.login;
 
-import io.qameta.allure.Description;
-import io.qameta.allure.Severity;
-import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.*;
+import io.qameta.allure.testng.Tag;
 import io.qase.api.annotation.QaseId;
 import io.qase.api.annotation.QaseTitle;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import qa.enums.URLs;
+import qa.support.constans.URLs;
 import qa.pageobject.loginform.LoginForm;
 import tests.base.BaseTest;
 
 public class InputTextVerificationTest extends BaseTest {
 
     private LoginForm loginForm;
-    private final String TEXT = "This is the text!";
+    private final String text = "This is the text!";
 
     @BeforeMethod
     public void create() {
 
-        goToPage(URLs.LOGIN_PAGE.getName());
+        goToPage(URLs.LOGIN_PAGE);
         loginForm = new LoginForm(getDriver());
     }
 
     @Test(priority = 1)
+
     @Severity(SeverityLevel.CRITICAL)
+    @Owner("Paweł Aksman")
+    @Tag("Login")
+    @Tag("Fields")
+    @Link(name = "Login page", value = URLs.LOGIN_PAGE)
     @QaseId(61)
     @QaseTitle("Verification of the \"Username or email address\" field text input")
     @Description("Verification of the \"Username or email address\" field text input")
     public void usernameField() throws IllegalAccessException {
 
-        loginForm.setUsername(TEXT);
-        Assert.assertEquals(loginForm.getUsername(), TEXT, "Incorrect the \"Username or email address\" field output");
+        Allure.parameter("Text", text);
+
+        loginForm.setUsername(text);
+        Assert.assertEquals(loginForm.getUsername(), text, "Incorrect the \"Username or email address\" field output");
     }
 
     @Test(priority = 2)
+    @Owner("Paweł Aksman")
+    @Tag("Login")
+    @Tag("Fields")
+    @Link(name = "Login page", value = URLs.LOGIN_PAGE)
     @Severity(SeverityLevel.CRITICAL)
     @QaseId(62)
     @QaseTitle("Verification of the \"Password\" field text input")
     @Description("Verification of the \"Password\" field text input")
     public void emailField() throws IllegalAccessException {
 
-        loginForm.setPassword(TEXT);
-        Assert.assertEquals(loginForm.getPassword(), TEXT, "Incorrect the \"Password\" field output");
+        Allure.parameter("Text", text);
+
+        loginForm.setPassword(text);
+        Assert.assertEquals(loginForm.getPassword(), text, "Incorrect the \"Password\" field output");
     }
 }
