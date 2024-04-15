3475b39da4895f032455415264199b61e8624780
pawelaksman <pablo_xmen@op.pl>
2024-04-16 00:07:20 +0200

    Added annotations

--- src/test/java/tests/login/LoginTest.java
+++ src/test/java/tests/login/LoginTest.java
@@ -1,8 +1,7 @@
 package tests.login;
 
-import io.qameta.allure.Description;
-import io.qameta.allure.Severity;
-import io.qameta.allure.SeverityLevel;
+import io.qameta.allure.*;
+import io.qameta.allure.testng.Tag;
 import io.qase.api.annotation.QaseId;
 import io.qase.api.annotation.QaseTitle;
 import tests.base.BaseTest;
@@ -10,7 +9,7 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import qa.dataproviders.CredentialsDataProviders;
-import qa.enums.URLs;
+import qa.support.constans.URLs;
 import qa.pageobject.account.AccountNavigation;
 import qa.pageobject.loginform.LoginForm;
 import qa.models.Credentials;
@@ -23,8 +22,14 @@ public class LoginTest extends BaseTest {
     @BeforeMethod
     private void create() {
 
-        goToPage(URLs.LOGIN_PAGE.getName());
+        goToPage(URLs.LOGIN_PAGE);
         loginForm = new LoginForm(getDriver());
+    }
+
+    private void setAllureParameters(Credentials credentials) {
+
+        Allure.parameter("Username", credentials.getEmailOrUsername());
+        Allure.parameter("Password", credentials.getPassword());
     }
 
     private void waitForErrorMessage() {
@@ -52,69 +57,109 @@ public class LoginTest extends BaseTest {
     }
 
     @Test(priority = 1, dataProvider = DataProviderNames.INCORRECT_USERNAME, dataProviderClass = CredentialsDataProviders.class)
+    @Owner("Paweł Aksman")
+    @Tag("Login")
+    @Tag("Fields")
+    @Link(name = "Login page", value = URLs.LOGIN_PAGE)
     @Severity(SeverityLevel.CRITICAL)
     @QaseId(63)
     @QaseTitle("Attempting to log in using an incorrect username or email address")
     @Description("Attempting to log in using an incorrect username or email address")
     public void incorrectUsername(Credentials credentials) throws IllegalAccessException {
 
-        loginForm.setUsername(credentials.getEmailOrUsername());
-        loginForm.setPassword(credentials.getPassword());
-        loginForm.clickLoginButton();
+        setAllureParameters(credentials);
+
+        loginForm
+                .setUsername(credentials.getEmailOrUsername())
+                .setPassword(credentials.getPassword())
+                .clickLoginButton();
+
         waitForErrorMessage();
         checkErrorMessageContent(credentials.getMessage());
     }
 
     @Test(priority = 4, dataProvider = DataProviderNames.BLANK_USERNAME_FIELD, dataProviderClass = CredentialsDataProviders.class)
+    @Owner("Paweł Aksman")
+    @Tag("Login")
+    @Tag("Fields")
+    @Link(name = "Login page", value = URLs.LOGIN_PAGE)
     @Severity(SeverityLevel.CRITICAL)
     @QaseId(64)
     @QaseTitle("Attempting to log in without providing input for the \"Username or email address\" field")
     @Description("Attempting to log in without providing input for the \"Username or email address\" field")
     public void blankUsernameField(Credentials credentials) throws IllegalAccessException {
 
-        loginForm.setPassword(credentials.getPassword());
-        loginForm.clickLoginButton();
+        setAllureParameters(credentials);
+
+        loginForm
+                .setPassword(credentials.getPassword())
+                .clickLoginButton();
+
         waitForErrorMessage();
         checkErrorMessageContent(credentials.getMessage());
     }
 
     @Test(priority = 3, dataProvider = DataProviderNames.INCORRECT_PASSWORD, dataProviderClass = CredentialsDataProviders.class)
+    @Owner("Paweł Aksman")
+    @Tag("Login")
+    @Tag("Fields")
+    @Link(name = "Login page", value = URLs.LOGIN_PAGE)
     @Severity(SeverityLevel.CRITICAL)
     @QaseId(65)
     @QaseTitle("Attempting to log in using an incorrect password")
     @Description("Attempting to log in using an incorrect password")
     public void incorrectPassword(Credentials credentials) throws IllegalAccessException {
 
-        loginForm.setUsername(credentials.getEmailOrUsername());
-        loginForm.setPassword(credentials.getPassword());
-        loginForm.clickLoginButton();
+        setAllureParameters(credentials);
+
+        loginForm
+                .setUsername(credentials.getEmailOrUsername())
+                .setPassword(credentials.getPassword())
+                .clickLoginButton();
+
         waitForErrorMessage();
         checkErrorMessageContent(credentials.getMessage());
     }
 
     @Test(priority = 5, dataProvider = DataProviderNames.BLANK_PASSWORD_FIELD, dataProviderClass = CredentialsDataProviders.class)
+    @Owner("Paweł Aksman")
+    @Tag("Login")
+    @Tag("Fields")
+    @Link(name = "Login page", value = URLs.LOGIN_PAGE)
     @Severity(SeverityLevel.CRITICAL)
     @QaseId(66)
     @QaseTitle("Attempting to log in without providing input for the \"Password\" field")
     @Description("Attempting to log in without providing input for the \"Password\" field")
     public void blankPasswordField(Credentials credentials) throws IllegalAccessException {
 
-        loginForm.setUsername(credentials.getEmailOrUsername());
-        loginForm.clickLoginButton();
+        setAllureParameters(credentials);
+
+        loginForm
+                .setUsername(credentials.getEmailOrUsername())
+                .clickLoginButton();
+
         waitForErrorMessage();
         checkErrorMessageContent(credentials.getMessage());
     }
 
     @Test(priority = 2, dataProvider = DataProviderNames.CORRECT, dataProviderClass = CredentialsDataProviders.class)
+    @Owner("Paweł Aksman")
+    @Tag("Login")
+    @Tag("Fields")
+    @Link(name = "Login page", value = URLs.LOGIN_PAGE)
     @Severity(SeverityLevel.CRITICAL)
     @QaseId(67)
     @QaseTitle("Logging in using correct credentials")
     @Description("Logging in using correct credentials")
     public void correct(Credentials credentials) throws IllegalAccessException {
 
-        loginForm.setUsername(credentials.getEmailOrUsername());
-        loginForm.setPassword(credentials.getPassword());
-        loginForm.clickLoginButton();
+        setAllureParameters(credentials);
+
+        loginForm
+                .setUsername(credentials.getEmailOrUsername())
+                .setPassword(credentials.getPassword())
+                .clickLoginButton();
+
         waitForAccountPage();
     }
 }
