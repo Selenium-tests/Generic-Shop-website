7358a32af06aaf19ee1dce1273494062b3267605
pawelaksman <pablo_xmen@op.pl>
2024-04-16 22:12:13 +0200

    Added the method

--- src/main/java/qa/listener/TestListener.java
+++ src/main/java/qa/listener/TestListener.java
@@ -1,14 +1,31 @@
 package qa.listener;
 
+import io.qameta.allure.Attachment;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.ITestContext;
 import org.testng.ITestListener;
 import org.testng.ITestResult;
+import qa.driver.WebDriverGlobal;
+
 
 public class TestListener implements ITestListener {
 
     private final Logger logger = LoggerFactory.getLogger(TestListener.class);
+
+    @Attachment(value = "{0}", type = "text/plain")
+    public static String saveTextLog(String message) {
+
+        return message;
+    }
+
+    @Attachment(value = "Screenshot", type = "image/png")
+    public byte[] takeScreenshot() {
+
+        return ((TakesScreenshot) WebDriverGlobal.getDriver()).getScreenshotAs(OutputType.BYTES);
+    }
 
     @Override
     public void onStart(ITestContext iTestContext) {
@@ -25,7 +42,7 @@ public class TestListener implements ITestListener {
     @Override
     public void onTestStart(ITestResult iTestResult) {
 
-        logger.info("Starting test omn method: " + iTestResult.getMethod().getMethodName());
+        logger.info("Starting test on method: " + iTestResult.getMethod().getMethodName());
     }
 
     @Override
@@ -37,6 +54,8 @@ public class TestListener implements ITestListener {
     @Override
     public void onTestFailure(ITestResult iTestResult) {
 
+        takeScreenshot();
+        saveTextLog(iTestResult.getThrowable().getMessage());
         logger.error("Test FAILED: " + iTestResult.getThrowable().getMessage());
     }
 
