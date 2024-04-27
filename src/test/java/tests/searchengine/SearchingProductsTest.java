2e16aad3c0d32503095d484df87ac4e952e51c6e
pawelaksman <pablo_xmen@op.pl>
2024-04-27 21:28:34 +0200

    Updated

--- src/test/java/tests/searchengine/SearchingProductsTest.java
+++ src/test/java/tests/searchengine/SearchingProductsTest.java
@@ -10,33 +10,30 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import qa.dataproviders.PhrasesDataProviders;
 import qa.support.constans.URLs;
-import qa.pageobject.header.ResultsPage;
+import qa.pageobject.header.SearchResultsPage;
 import qa.pageobject.header.SearchEngine;
 import qa.support.constans.DataProviderNames;
 
-import java.util.function.Consumer;
 
 @Epic("E2E")
 @Feature("Searching products")
 public class SearchingProductsTest extends BaseTest {
 
     private SearchEngine searchEngine;
-    private ResultsPage resultsPage;
+    private SearchResultsPage searchResultsPage;
 
     @BeforeMethod
     private void prepare() {
 
         goToPage(URLs.HOME_PAGE);
         searchEngine = new SearchEngine(getDriver());
-        resultsPage = new ResultsPage(getDriver());
+        searchResultsPage = new SearchResultsPage(getDriver());
     }
 
-    private void check(String phrase, Consumer<ResultsPage> consumer) throws IllegalAccessException {
+    private void action(String phrase) throws IllegalAccessException {
 
         searchEngine.setPhrase(phrase);
         searchEngine.clickSubmitButton();
-
-        consumer.accept(resultsPage);
     }
 
     @Test(priority = 1, dataProvider = DataProviderNames.CORRECT, dataProviderClass = PhrasesDataProviders.class)
@@ -49,12 +46,13 @@ public class SearchingProductsTest extends BaseTest {
     @Description("Searching with a correct phrase")
     @QaseId(6)
     @QaseTitle("Searching with a correct phrase")
-    public void correct(String phrase) throws IllegalAccessException {
+    public void correct(String phrase) throws IllegalAccessException, InterruptedException {
 
         Allure.parameter("Phrase", phrase);
 
-        check(phrase, (ResultsPage rp)-> Assert.assertFalse(rp.hasNoResults(),
-                "No results when searching with the \"" + phrase + "\" phrase"));
+        action(phrase);
+        Assert.assertTrue(searchResultsPage.getNumberOfArticles() > 0,
+                "No results when searching with the \"" + phrase + "\" phrase");
     }
 
     @Test(priority = 2, dataProvider = DataProviderNames.INCORRECT, dataProviderClass = PhrasesDataProviders.class)
@@ -66,11 +64,12 @@ public class SearchingProductsTest extends BaseTest {
     @Description("Searching with an incorrect phrase")
     @QaseId(7)
     @QaseTitle("Searching with an incorrect phrase")
-    public void incorrect(String phrase) throws IllegalAccessException {
+    public void incorrect(String phrase) throws IllegalAccessException, InterruptedException {
 
         Allure.parameter("Phrase", phrase);
 
-        check(phrase, (ResultsPage rp)-> Assert.assertTrue(rp.hasNoResults(),
-                "Results found when searching with \"" + phrase + "\" as an incorrect phrase"));
+        action(phrase);
+        Assert.assertEquals(searchResultsPage.getNumberOfArticles(), 0,
+                "Results found when searching with \"" + phrase + "\" as an incorrect phrase");
     }
 }
